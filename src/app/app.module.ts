55bcdb7755385c2a17806e14c3835ec7e80a5a20
Janne Mäki <janne.maki@evident.fi>
2017-01-10 15:55:09 +0200

    Use bootstrap styles and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './components/app.component';
 import { TermedService } from './services/termed.service';
@@ -13,7 +14,8 @@ import { TermedService } from './services/termed.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
   ],
   providers: [TermedService],
   bootstrap: [AppComponent]
