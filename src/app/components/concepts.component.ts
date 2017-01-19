1c6e05153db8d8d976d41a34ec1ba744f658b002
Janne Mäki <janne.maki@evident.fi>
2017-01-19 17:32:38 +0200

    Filtering concepts by name implemented

--- src/app/components/concepts.component.ts
+++ src/app/components/concepts.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TermedService, ConceptListItem } from '../services/termed.service';
 import { ActivatedRoute } from '@angular/router';
 import { LocationService } from '../services/location.service';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { LanguageService } from '../services/language.service';
 
 @Component({
   selector: 'concepts',
@@ -13,20 +15,33 @@ import { LocationService } from '../services/location.service';
         <div class="col-md-12">
           <div class="page-header">
             <h1 translate>Concepts</h1>
-          </div>        
+          </div>
         </div>
       </div>
       <div class="row">
-        <div class="col-md-12">
-          <ul *ngIf="concepts">
-            <li *ngFor="let concept of concepts">
-              <a [routerLink]="['concept', concept.id]">
-                {{concept.label | translateValue}}
-              </a>
-            </li>
-          </ul>
-          <ajax-loading-indicator *ngIf="!concepts"></ajax-loading-indicator>
-        </div>
+          <div class="col-md-4">
+            <div class="input-group input-group-lg">
+              <input (keydown)="onSearch($event.target.value)"
+                     type="text" 
+                     class="form-control" 
+                     [placeholder]="'search...' | translate" />
+            </div>
+          </div>
+          <div class="col-md-8">
+            
+            <ul *ngIf="!loading">
+              <li *ngFor="let concept of searchResults | async">
+                <a [routerLink]="['concept', concept.id]">
+                  {{concept.label | translateValue}}
+                </a>
+              </li>
+            </ul>
+            
+            <div *ngIf="loading">
+              <ajax-loading-indicator></ajax-loading-indicator>
+            </div>
+            
+          </div>
       </div>
       
     </div>
@@ -34,16 +49,40 @@ import { LocationService } from '../services/location.service';
 })
 export class ConceptsComponent implements OnInit {
 
-  concepts: ConceptListItem[];
+  loading = true;
+  searchResults: Observable<ConceptListItem[]>;
+  search$ = new BehaviorSubject('');
 
-  constructor(private route: ActivatedRoute, private termedService: TermedService, private locationService: LocationService) {
+  constructor(private route: ActivatedRoute,
+              private termedService: TermedService,
+              private locationService: LocationService,
+              private languageService: LanguageService) {
+  }
+
+  onSearch(value: string) {
+    this.search$.next(value);
   }
 
   ngOnInit() {
+
+    const concepts = this.route.params.switchMap(params => this.termedService.getConceptListItems(params['graphId']));
+
+    this.searchResults = Observable.combineLatest([concepts, this.search$], (concepts: ConceptListItem[], search: string) => {
+
+      // TODO: use levenshtein for matching and sorting
+      return concepts.filter(concept => {
+        if (!search) {
+          return true;
+        } else {
+          const localized = this.languageService.translate(concept.label);
+          return localized.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+        }
+      });
+    });
+
     this.route.params.switchMap(params => this.termedService.getConceptScheme(params['graphId']))
       .subscribe(scheme => this.locationService.atConceptScheme(scheme));
 
-    this.route.params.switchMap(params => this.termedService.getConceptListItems(params['graphId']))
-      .subscribe(concepts => this.concepts = concepts);
+    concepts.subscribe(() => this.loading = false);
   }
 }
