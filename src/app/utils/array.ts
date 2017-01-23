9d7ac07b81c7fbe6c291b752fc1e477b2607eeae
Janne Mäki <janne.maki@evident.fi>
2017-01-23 12:54:20 +0200

    More utility functions copied from iow

--- src/app/utils/array.ts
+++ src/app/utils/array.ts
@@ -1,7 +1,37 @@
-import { isDefined } from './object';
+import { isDefined, EqualityChecker } from './object';
+
+export function limit<T>(arr: T[], limit: number): T[] {
+  return arr.slice(0, Math.min(limit, arr.length));
+}
+
+export function filterDefined<T>(arr: (T|null|undefined)[]): T[] {
+  const result: T[] = [];
+
+  for (const item of arr) {
+    if (isDefined(item)) {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
 
 export function normalizeAsArray<T>(obj: T|T[]): T[] {
   return Array.isArray(obj) ? obj : isDefined(obj) ? [obj] : [];
+}
+
+export function collectProperties<T, TResult>(items: T[]|T[][], propertyExtractor: (item: T) => TResult): Set<TResult> {
+  const result = new Set<TResult>();
+  for (const item of items) {
+    if (Array.isArray(item)) {
+      for (const innerItem of item) {
+        result.add(propertyExtractor(innerItem));
+      }
+    } else {
+      result.add(propertyExtractor(item));
+    }
+  }
+  return result;
 }
 
 export function index<T, TIndex>(items: T[], indexExtractor: (item: T) => TIndex): Map<TIndex, T> {
@@ -14,12 +44,87 @@ export function index<T, TIndex>(items: T[], indexExtractor: (item: T) => TIndex
   return result;
 }
 
-export function filterDefined<T>(items: (T|undefined)[]): T[] {
+export function referenceEquality<T>(lhs: T, rhs: T) {
+  return lhs === rhs;
+}
+
+export function any<T>(arr: T[], predicate: (item: T) => boolean) {
+  for (const item of arr) {
+    if (predicate(item)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export function all<T>(arr: T[], predicate: (item: T) => boolean) {
+  for (const item of arr) {
+    if (!predicate(item)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function first<T>(arr: T[], predicate: (item: T) => boolean): T|null {
+  for (const item of arr) {
+    if (predicate(item)) {
+      return item;
+    }
+  }
+  return null;
+}
+
+export function contains<T>(arr: T[], value: T, equals: EqualityChecker<T> = referenceEquality): boolean {
+  return any(arr, (item: T) => equals(item, value));
+}
+
+export function containsAny<T>(arr: T[], values: T[], equals: EqualityChecker<T> = referenceEquality): boolean {
+  return any(values, (value: T) => contains(arr, value, equals));
+}
+
+export function containsAll<T>(arr: T[], values: T[], equals: EqualityChecker<T> = referenceEquality): boolean {
+  return all(values, (value: T) => contains(arr, value, equals));
+}
+
+export function arraysAreEqual<T>(lhs: T[], rhs: T[], equals: EqualityChecker<T> = referenceEquality) {
+  return containsAll(lhs, rhs, equals) && containsAll(rhs, lhs, equals);
+}
+
+export function findFirstMatching<T>(arr: T[], values: T[], equals: EqualityChecker<T> = referenceEquality): T|null {
+  return first(arr, item => contains(values, item, equals));
+}
+
+export function removeMatching<T>(arr: T[], predicate: (item: T) => boolean) {
+
+  const matchingIndices: number[] = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const item = arr[i];
+    if (predicate(item)) {
+      matchingIndices.push(i);
+    }
+  }
+
+  for (const index of matchingIndices) {
+    arr.splice(index, 1);
+  }
+}
+
+export function keepMatching<T>(arr: T[], predicate: (item: T) => boolean) {
+  removeMatching(arr, item => !predicate(item));
+}
+
+export function remove<T>(arr: T[], item: T): void {
+  removeMatching(arr, i => i === item);
+}
+
+export function flatten<T>(arr: T[][]) {
 
   const result: T[] = [];
 
-  for (const item of items) {
-    if (item) {
+  for (const innerArr of arr) {
+    for (const item of innerArr) {
       result.push(item);
     }
   }
@@ -27,15 +132,33 @@ export function filterDefined<T>(items: (T|undefined)[]): T[] {
   return result;
 }
 
+export function groupBy<T, I>(arr: T[], indexByExtractor: (item: T) => I): Map<I, T[]> {
 
-export function flatten<T>(arr: T[][]): T[] {
-  const result: T[] = [];
+  const result = new Map<I, T[]>();
 
-  for (const outer of arr) {
-    for (const inner of outer) {
-      result.push(inner);
+  function createOrGet(indexProperty: I): T[] {
+    const resultList = result.get(indexProperty);
+
+    if (resultList) {
+      return resultList;
+    } else {
+      const newResultList: T[] = [];
+      result.set(indexProperty, newResultList);
+      return newResultList;
     }
   }
 
+  for (const item of arr) {
+    createOrGet(indexByExtractor(item)).push(item);
+  }
+
   return result;
+}
+
+export function requireSingle<T>(arr: T[]): T {
+  if (arr.length !== 1) {
+    throw new Error('Single element required, was: ' + arr.length);
+  }
+
+  return arr[0];
 }
--- src/app/utils/object.ts
+++ src/app/utils/object.ts
@@ -1,5 +1,21 @@
 export type Optional<T> = T|null|undefined;
 
+export type EqualityChecker<T> = (lhs: T, rhs: T) => boolean;
+
+export function referenceEquality<T>(lhs: T, rhs: T) {
+  return lhs === rhs;
+}
+
+export function areEqual<T>(lhs: Optional<T>, rhs: Optional<T>, equals: EqualityChecker<T> = referenceEquality): boolean {
+  if ((isDefined(lhs) && !isDefined(rhs)) || (!isDefined(lhs) && isDefined(rhs))) {
+    return false;
+  } else if (!isDefined(lhs) && !isDefined(rhs)) {
+    return true;
+  } else {
+    return equals(lhs!, rhs!);
+  }
+}
+
 export function isDefined<T>(obj: Optional<T>): obj is T {
   return obj !== null && obj !== undefined;
 }
