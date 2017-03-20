6e9f60d3bccccd3e09c46d174a3d2a2c9089cf66
Janne Mäki <janne.maki@evident.fi>
2017-03-20 14:48:10 +0200

    Hide breadcrumb for front page

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { LocationService } from '../services/location.service';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
   template: `
     <navigation-bar></navigation-bar>
-    <breadcrumb></breadcrumb>
+    <breadcrumb [hidden]="!showBreadcrumb"></breadcrumb>
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
+
+  showBreadcrumb: boolean;
+
+  constructor(locationService: LocationService) {
+    locationService.location.subscribe(location => {
+      this.showBreadcrumb = location.length > 1;
+    })
+  }
 }
