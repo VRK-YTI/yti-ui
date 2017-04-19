0525d1f6d22c91c48d36a1d432e6bc5c1bd18ccb
Janne Mäki <janne.maki@evident.fi>
2017-04-19 13:23:48 +0300

    New concept can be created also from hierarchical listing

--- src/app/components/concept/concept-hierarchy.component.ts
+++ src/app/components/concept/concept-hierarchy.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ConceptViewModelService } from '../../services/concept.view.service';
+import { v4 as uuid } from 'uuid';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'concept-hierarchy',
@@ -7,6 +9,14 @@ import { ConceptViewModelService } from '../../services/concept.view.service';
   template: `
     <div class="row">
       <div class="col-lg-12 tree">
+
+        <div class="actions">
+          <button class="button btn-default btn-add-new" (click)="addConcept()">
+            <i class="fa fa-plus"></i>
+            <span translate>Add concept</span>
+          </button>
+        </div>
+        
         <ul>
           <li *ngFor="let concept of topConcepts | async">
             <concept-hierarchy-node [concept]="concept"></concept-hierarchy-node>
@@ -18,10 +28,15 @@ import { ConceptViewModelService } from '../../services/concept.view.service';
 })
 export class ConceptHierarchyComponent {
 
-  constructor(private conceptViewModel: ConceptViewModelService) {
+  constructor(private router: Router,
+              private conceptViewModel: ConceptViewModelService) {
   }
 
   get topConcepts() {
     return this.conceptViewModel.topConcepts$;
+  }
+
+  addConcept() {
+    this.router.navigate(['/concepts', this.conceptViewModel.graphId, 'concept', uuid()]);
   }
 }
