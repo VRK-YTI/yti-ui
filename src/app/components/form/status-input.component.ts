2bd92d8a465ba4dedb17e6d1be2445560226e28b
Janne Mäki <janne.maki@evident.fi>
2017-05-09 13:57:00 +0300

    Fix template error when starting edit

--- src/app/components/form/status-input.component.ts
+++ src/app/components/form/status-input.component.ts
@@ -34,7 +34,7 @@ export class StatusInputComponent {
   }
 
   get valueInError() {
-    return this.ngModel && this.ngModel.errors;
+    return !this.ngModel || this.ngModel.valid;
   }
 
   get editing() {
