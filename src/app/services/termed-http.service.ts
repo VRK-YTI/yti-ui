2742a73e82b3f4ebc4d664ab67bb05772214bf58
Janne Mäki <janne.maki@evident.fi>
2017-08-17 18:12:55 +0300

    Use frontend api instead of calling termed-api directly

--- src/app/services/termed-http.service.ts
+++ src/app/services/termed-http.service.ts
@@ -1,22 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, RequestOptionsArgs, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
-import { UserService } from './user.service';
 
 export interface UserCredentials {
   username: string;
   password: string;
 }
 
-const readOnlyCredentials: UserCredentials = {
-  username: 'admin',
-  password: 'admin'
-};
-
 @Injectable()
 export class TermedHttp {
 
-  constructor(private http: Http, private userService: UserService) {
+  constructor(private http: Http) {
   }
 
   get(url: string, options?: RequestOptionsArgs): Observable<Response> {
@@ -35,12 +29,6 @@ export class TermedHttp {
     return this.http.delete(url, this.authorize(options));
   }
 
-  static addAuthorizationHeader(headers: Headers, credentials: UserCredentials) {
-    if (!headers.has('Authorization')) {
-      headers.append('Authorization', `Basic ${btoa(`${credentials.username}:${credentials.password}`)}`);
-    }
-  }
-
   private authorize(options?: RequestOptionsArgs) {
 
     const opts = options || {};
@@ -50,8 +38,6 @@ export class TermedHttp {
     }
 
     opts.headers.append('X-Requested-With', 'XMLHttpRequest');
-
-    TermedHttp.addAuthorizationHeader(opts.headers, this.userService.isLoggedIn() ? this.userService.user! : readOnlyCredentials);
 
     return opts;
   }
