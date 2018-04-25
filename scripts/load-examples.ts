f06cf27843efb4d9dbe269b776deba3fb191ed02
Janne Mäki <janne.maki@evident.fi>
2018-04-25 10:53:06 +0300

    Load examples using terminology-api instead of termed directly

--- scripts/load-examples.ts
+++ scripts/load-examples.ts
@@ -1,34 +1,20 @@
 import { default as fetch, Response } from 'node-fetch';
 
-const jhsGraph = require('../examples/jhsGraph.json');
-const jhsTypes = require('../examples/jhsTypes.json');
-const jhsNodes = require('../examples/jhsNodes.json');
+const jhs = require('../examples/jhs.json');
+const jhsVocabulary = require('../examples/jhs-vocabulary.json');
+const oksa = require('../examples/oksa.json');
+const oksaVocabulary = require('../examples/oksa-vocabulary.json');
+const sos = require('../examples/sos.json');
+const sosVocabulary = require('../examples/sos-vocabulary.json');
+const kira = require('../examples/kira.json');
+const kiraVocabulary = require('../examples/kira-vocabulary.json');
 
-const oksaGraph = require('../examples/oksaGraph.json');
-const oksaTypes = require('../examples/oksaTypes.json');
-const oksaNodes = require('../examples/oksaNodes.json');
-
-const sosGraph = require('../examples/sosGraph.json');
-const sosTypes = require('../examples/sosTypes.json');
-const sosNodes = require('../examples/sosNodes.json');
-
-const kiraGraph = require('../examples/kiraGraph.json');
-const kiraTypes = require('../examples/kiraTypes.json');
-const kiraNodes = require('../examples/kiraNodes.json');
-
-const username = 'admin';
-const password = 'admin';
-
-function btoa(s: string): string {
-  return new Buffer(s).toString('base64');
-}
-
-const endpoint = process.argv.length > 2 ? process.argv[2] : 'http://localhost:8080/api';
+const mail = 'ytitestaaja@gmail.com';
+const endpoint = process.argv.length > 2 ? process.argv[2] : 'http://localhost:8001/frontend';
 
 const headers = {
   'Accept': 'application/json',
-  'Content-Type': 'application/json',
-  'Authorization': `Basic ${btoa(`${username}:${password}`)}`
+  'Content-Type': 'application/json'
 };
 
 function handleErrors(response: Response): Response {
@@ -38,28 +24,34 @@ function handleErrors(response: Response): Response {
   return response;
 }
 
-function createGraph(graph: any): Promise<any> {
-  return fetch(`${endpoint}/graphs`, { headers, method: 'POST', body: JSON.stringify(graph) }).then(handleErrors);
-}
+function createVocabulary(prefix: string, type: 'Vocabulary'|'TerminologicalVocabulary', vocabularyNode: any): Promise<any> {
 
-function createTypes(graphId: any, types: any): Promise<any> {
-  return fetch(`${endpoint}/graphs/${graphId}/types?batch=true`, { headers, method: 'POST', body: JSON.stringify(types) }).then(handleErrors);
+  const templateGraphId = type === 'Vocabulary' ? 'b387af64-4c66-4542-95f3-4c33c6831fcc' : '61cf6bde-46e6-40bb-b465-9b2c66bf4ad8';
+  const graphId = vocabularyNode.type.graph.id;
+  const url = `${endpoint}/vocabulary?sync=false&templateGraphId=${templateGraphId}&prefix=${prefix}&graphId=${graphId}&fake.login.mail=${mail}`;
+
+  return fetch(url, { headers, method: 'POST', body: JSON.stringify(vocabularyNode) }).then(handleErrors);
 }
 
 function createNodes(nodes: any): Promise<any> {
-  return fetch(`${endpoint}/nodes?batch=true`, { headers, method: 'POST', body: JSON.stringify(nodes) }).then(handleErrors);
+
+  const body = {
+    delete: [],
+    save: nodes
+  };
+
+  return fetch(`${endpoint}/modify?sync=false&fake.login.mail=${mail}`, { headers, method: 'POST', body: JSON.stringify(body) }).then(handleErrors);
 }
 
-function initData(name: string, graph: any, types: any, nodes?: any): Promise<any> {
-  return createGraph(graph)
-    .then(() => createTypes(graph.id, types))
-    .then(() => Promise.resolve(nodes ? createNodes(nodes) : ''))
-    .then(() => console.log(name));
+function initData(prefix: string, type: 'Vocabulary'|'TerminologicalVocabulary', vocabularyNode: any, nodes: any): Promise<any> {
+  return createVocabulary(prefix, type, vocabularyNode)
+    .then(() => createNodes(nodes))
+    .then(() => console.log(prefix));
 }
 
 Promise.all([
-  initData('JHS', jhsGraph, jhsTypes, jhsNodes),
-  initData('SOS', sosGraph, sosTypes, sosNodes),
-  initData('OKSA', oksaGraph, oksaTypes, oksaNodes),
-  initData('KIRA', kiraGraph, kiraTypes, kiraNodes)
+  initData('jhs', 'TerminologicalVocabulary', jhsVocabulary, jhs),
+  initData('sos', 'TerminologicalVocabulary', sosVocabulary, sos),
+  initData('oksa', 'TerminologicalVocabulary', oksaVocabulary, oksa),
+  initData('kira', 'TerminologicalVocabulary', kiraVocabulary, kira)
 ]).then(() => console.log('========\nOK'));
