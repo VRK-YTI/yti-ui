7565be6f258f1728c7e60f22e97f67ef72137f6f
Tuomo Soinio <tuomo.soinio@gofore.com>
2018-10-25 11:23:47 +0300

    YTI-1042: Continue spinning loading indicator while XML is being processed.

--- src/app/components/vocabulary/import-vocabulary-xml.component.ts
+++ src/app/components/vocabulary/import-vocabulary-xml.component.ts
@@ -2,17 +2,17 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { VocabularyNode } from 'app/entities/node';
 import { importApiUrl } from '../../config';
-import { HttpClient, HttpEventType, HttpRequest, HttpResponseBase } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpRequest, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-import-vocabulary-xml',
   styleUrls: ['./import-vocabulary-xml.component.scss'],
   template: `
-    <div *ngIf="uploading">
+    <div *ngIf="uploading || processing">
       <app-ajax-loading-indicator></app-ajax-loading-indicator>
     </div>
 
-    <div *ngIf="!uploading">
+    <div *ngIf="!uploading && !processing">
       <div class="modal-header">
         <h4 class="modal-title">
           <a><i class="fa fa-times" id="cancel_import_link" (click)="cancel()"></i></a>
@@ -40,6 +40,7 @@ export class ImportVocabularyXMLComponent implements OnInit {
   @Input() importFile: File;
 
   uploading = false;
+  processing = false;
   status?: string;
   errorMessage?: string;
 
@@ -52,14 +53,23 @@ export class ImportVocabularyXMLComponent implements OnInit {
     // TODO: Move upload code to a service!
     const data = new FormData();
     data.append('file', this.importFile);
-    this.http.request(new HttpRequest('POST', importApiUrl + '/ntrf/' + this.vocabulary.graphId, data, { reportProgress: true })).subscribe(
+    this.http.request(new HttpRequest('POST', importApiUrl + '/ntrf/' + this.vocabulary.graphId, data, {
+      reportProgress: true,
+      responseType: 'json'
+    })).subscribe(
       event => {
         if (event.type == HttpEventType.UploadProgress && event.total) {
           // const percentDone = Math.round(100 * event.loaded / event.total);
-        } else if (event instanceof HttpResponseBase) {
+        } else if (event instanceof HttpResponse) {
           this.status = event.status + ' ' + event.statusText;
+          if (event.status === 200 && event.body) {
+            const anybody = <any> event.body;
+            if (anybody.jobtoken) {
+              this.processing = true;
+              this.pollProgress(anybody.jobtoken);
+            }
+          }
         }
-
       },
       err => {
         const error = err && err.error && err.error.error;
@@ -84,5 +94,33 @@ export class ImportVocabularyXMLComponent implements OnInit {
 
   confirm() {
     this.modal.close();
+  }
+
+  pollProgress(token: string) {
+    this.http.get(importApiUrl + '/status/' + token, {
+      responseType: 'json'
+    }).subscribe(event => {
+        const anybody = <any> event;
+        if (anybody.status === 'Processing') {
+          setTimeout(() => this.pollProgress(token), 500);
+        } else if (anybody.status === 'Ready') {
+          this.processing = false;
+        } else {
+          this.progressError();
+        }
+      },
+      err => {
+        this.progressError();
+      },
+      () => {
+      }
+    );
+  }
+
+  progressError() {
+    if (!this.errorMessage) {
+      this.errorMessage = 'Monitoring failed. Import status unknown.';
+    }
+    this.processing = false;
   }
 }
