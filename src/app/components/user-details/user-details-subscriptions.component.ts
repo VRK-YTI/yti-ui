af7f5431d61ffdfd0e6a6537c2bbf577626f14b9
Antti Tohmo <antti.tohmo@gofore.com>
2019-11-15 14:50:21 +0200

    YTI-1107: Harmonizing subscriptions UI to latest Comments implementation that supports regular uris for subscription resources.

--- src/app/components/user-details/user-details-subscriptions.component.ts
+++ src/app/components/user-details/user-details-subscriptions.component.ts
@@ -171,20 +171,4 @@ export class UserDetailsSubscriptionsComponent implements OnInit {
 
     return this.configurationService.getUriWithEnv(uri);
   }
-
-  getCommentsUriWithEnv(uri: string): string | null {
-
-    switch (this.configurationService.environment) {
-      case 'awsprod':
-        return 'https://kommentit.suomi.fi/commentround;commentRoundId=' + uri;
-      case 'awstest':
-        return 'https://kommentit.dev.yti.cloud.vrk.fi/commentround;commentRoundId=' + uri;
-      case 'awsdev':
-        return 'https://kommentit.dev.yti.cloud.vrk.fi/commentround;commentRoundId=' + uri;
-      case 'local':
-        return 'http://localhost:9700/commentround;commentRoundId=' + uri;
-      default:
-        return this.getUriWithEnv(uri);
-    }
-  }
 }
