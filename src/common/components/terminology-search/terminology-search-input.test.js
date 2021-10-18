8673b53ce0fe514e2110721c01ea41414947da37
topirissanen-atoz <topi.rissanen@atoz.fi>
2021-10-18 14:59:18 +0300

    Removed unnecessary comments and added tests

--- src/common/components/terminology-search/terminology-search-input.test.js
+++ src/common/components/terminology-search/terminology-search-input.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
 import { TerminologySearchInput } from './terminology-search-input';
 
@@ -14,5 +15,23 @@ describe('TerminologySearchInput', () => {
 
     expect(screen.findByTestId('search_input')).toBeInTheDocument;
   });
+
+  test('should set filter', () => {
+
+    const setFilter = jest.fn();
+
+    render(
+      <TerminologySearchInput setFilter={setFilter} />
+    );
+
+    userEvent.click(screen.getByTestId('search_input'));
+    userEvent.keyboard('test');
+
+    expect(screen.getByTestId('search_input')).toBeInTheDocument;
+    expect(screen.getByTestId('search_input')).toHaveProperty('value', 'test')
+
+    userEvent.keyboard('{enter}');
+
+  })
 
 });
