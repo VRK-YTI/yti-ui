092142d4fa43092286566776747126d6d638300d
topirissanen-atoz <topi.rissanen@atoz.fi>
2021-10-19 12:23:15 +0300

    Added redux using redux-toolkit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,17 @@
-import { SWRConfig } from 'swr';
+import React from 'react';
 import '../../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
-import axios from 'axios';
-import React from 'react';
+import store from '../store';
+import { Provider } from 'react-redux'
 
 // https://nextjs.org/docs/advanced-features/custom-app
 function App({ Component, pageProps }: AppProps) {
 
   return (
-    <SWRConfig
-      value={{
-        fetcher: async (url: string, init?: RequestInit) => axios
-          .get(url)
-          .then(res => res.data),
-        onError: (err) => {
-          console.error(err);
-        },
-      }}
-    >
+    <Provider store={store}>
       <Component {...pageProps} />
-    </SWRConfig>
+    </Provider>
   );
 }
 export default appWithTranslation(App);
