35c0a15b3febfb410b8552e4733c7b5cc2af54ef
topirissanen-atoz <topi.rissanen@atoz.fi>
2021-10-19 14:57:30 +0300

    Current filter value is used as defaultValue

--- src/common/components/terminology-search/terminology-search-input.tsx
+++ src/common/components/terminology-search/terminology-search-input.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SearchInput } from 'suomifi-ui-components';
-import { useAppDispatch } from '../hooks';
+import { useAppDispatch, useAppSelector } from '../hooks';
 import { SearchContainer } from './terminology-search-input.styles';
-import { setFilter } from './terminology-search-slice'
+import { setFilter, selectFilter } from './terminology-search-slice'
 
 export function TerminologySearchInput() {
   const dispatch = useAppDispatch();
+  const filter = useAppSelector(selectFilter);
 
   return (
     <SearchContainer>
@@ -17,7 +18,8 @@ export function TerminologySearchInput() {
           if (typeof value === 'string') {
             dispatch(setFilter(value))
           }
-        } }
+        }}
+        defaultValue={filter}
       />
     </SearchContainer>
   );
