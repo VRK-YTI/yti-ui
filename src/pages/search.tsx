48580c39d2ba59d8da7b853db127449c4dfbc224
Urho Tamminen <urho.tamminen@gofore.com>
2021-11-01 15:54:38 +0200

    add common getServerSideProps wrapper
    Pages can use this wrapper to provide their own handler, while also
    automatically calling withSession() and serverSideTranslations().
    
    The common wrapper can also have additional code for adding props, which
    will then be merged with the page-specific props.
    
    Additionally this commit changes the i18n translation to always
    translate strings into a predictable "tr-translate-key".

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import React, { useState } from 'react';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { SSRConfig, useTranslation } from 'next-i18next';
 import { Heading } from 'suomifi-ui-components';
 import Layout from '../common/components/layout/layout';
@@ -8,10 +7,12 @@ import { TerminologySearchInput } from '../common/components/terminology-search/
 import useTerminologySearch from '../modules/terminology-search/hooks/terminology-search-api';
 import { TerminologySearchResults } from '../common/components/terminology-search/terminology-search-results';
 import { TerminologySearchResult } from '../common/interfaces/terminology.interface';
-import { UserProps } from '../common/interfaces/user-interface';
 import { SearchContainer } from '../common/components/terminology-search/terminology-search-input.styles';
-import withSession, { NextIronRequest } from '../common/utils/session';
-import { GetServerSideProps, NextApiResponse } from 'next';
+import { NextIronRequest } from '../common/utils/session';
+import { NextApiResponse } from 'next';
+import { createCommonGetServerSideProps } from '../common/utils/create-getserversideprops';
+import User from '../common/interfaces/user-interface';
+import useUser from '../common/hooks/useUser';
 
 interface TerminologySearchProps {
   results: TerminologySearchResult | null;
@@ -19,16 +20,18 @@ interface TerminologySearchProps {
   loading: boolean;
 }
 
-export default function Search(props: {
+export default function SearchPage(props: {
   _netI18Next: SSRConfig;
+  user: User;
 }) {
   const { t } = useTranslation('common');
+  const { user, } = useUser({ initialData: props.user });
 
   const [filter, setFilter] = useState<string | null>(null);
   const { results, error, loading }: TerminologySearchProps = useTerminologySearch(filter);
 
   return (
-    <Layout>
+    <Layout user={user}>
       <Head>
         <title>{t('search-title')}</title>
       </Head>
@@ -45,12 +48,7 @@ export default function Search(props: {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = withSession<{ props: UserProps | {} }>(
+export const getServerSideProps = createCommonGetServerSideProps(
   async ({ req, res, locale }: { req: NextIronRequest, res: NextApiResponse, locale: string }) => {
-    const props: any = {
-      ...(await serverSideTranslations(locale, ['common'])),
-    };
-
-    return { props: props };
-  },
-);
+    return { props: { } };
+  });
