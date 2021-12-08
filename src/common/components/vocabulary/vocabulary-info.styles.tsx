879142471538b5b690b38470a2e3dac618b9ba51
Jukka Hyytiälä <jukka.hyytiala@gofore.com>
2021-12-08 10:19:47 +0200

    Add MultilingualDefinitionList component
    Make reusable component for showing a list of values in multiple
    languages.
    
    YTI-1607

--- src/common/components/vocabulary/vocabulary-info.styles.tsx
+++ src/common/components/vocabulary/vocabulary-info.styles.tsx
@@ -6,56 +6,12 @@ export const InformationContainer = styled.div`
 
 export const NameWrapper = styled.div`
   margin-bottom: 16px;
-
-  ul {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-    margin-top: 12px;
-    border-top: solid 1px ${(props) => props.theme.suomifi.colors.depthLight1};
-    border-left: solid 1px ${(props) => props.theme.suomifi.colors.depthLight1};
-    border-right: solid 1px ${(props) => props.theme.suomifi.colors.depthLight1};
-    max-width: 70%;
-  }
-
-  b {
-    min-width: 50px;
-    display: inline-block;
-  }
-
-  li {
-    border-bottom: solid 1px ${(props) => props.theme.suomifi.colors.depthLight1};
-    padding-top: 6px;
-    padding-bottom: 6px;
-    padding-left: 12px;
-  }
-
-  li:nth-child(1) {
-    background-color: ${(props) => props.theme.suomifi.colors.whiteBase};
-  }
-
-  li:nth-child(2) {
-    background-color: ${(props) => props.theme.suomifi.colors.depthLight2};
-  }
+  max-width: 70%;
 `;
 
 export const DescriptionWrapper = styled.div`
   margin-bottom: 16px;
-
-  div {
-    border: solid 1px ${(props) => props.theme.suomifi.colors.depthLight1};
-    max-width: 70%;
-    margin-top: 6px;
-    padding-top: 6px;
-    padding-bottom: 6px;
-    display: flex;
-  }
-
-  b {
-    min-width: 50px;
-    display: inline-block;
-    padding-left: 16px;
-  }
+  max-width: 70%;
 `;
 
 export const InformationDomainWrapper = styled.div`
