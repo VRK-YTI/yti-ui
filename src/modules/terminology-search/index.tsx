5abf90ccfa08f9792075b30b481e4899a9d46c5e
topirissanen-atoz <82932696+topirissanen-atoz@users.noreply.github.com>
2021-12-10 07:06:22 +0200

    Yti 1606 pagination (#35)
    * Added pagination

--- src/modules/terminology-search/index.tsx
+++ src/modules/terminology-search/index.tsx
@@ -4,14 +4,27 @@ import { Grid } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import { Heading, Text } from 'suomifi-ui-components';
 import { TerminologySearchResults } from '../../common/components/terminology-search/terminology-search-results';
-import { selectFilter, useGetSearchResultQuery } from '../../common/components/terminology-search/terminology-search-slice';
+import { selectFilter, setResultStart, selectSearchFilter, useGetSearchResultQuery } from '../../common/components/terminology-search/terminology-search-slice';
 import TerminologySearchFilter from '../../common/components/terminology-search/terminology-search-filter';
 import { SearchCountWrapper } from '../../common/components/terminology-search/terminology-search.styles';
+import { useRouter } from 'next/router';
+import { useStoreDispatch } from '../../store';
 
 export default function TerminologySearch() {
   const { t } = useTranslation();
   const filter = useSelector(selectFilter());
-  const { data } = useGetSearchResultQuery(filter);
+  const searchFilter = useSelector(selectSearchFilter());
+  const { data } = useGetSearchResultQuery(searchFilter);
+  const query = useRouter();
+  const dispatch = useStoreDispatch();
+
+  if (query.query.page) {
+    if (query.query.page !== '1') {
+      dispatch(setResultStart((parseInt(query.query.page as string, 10) - 1) * 10));
+    } else {
+      dispatch(setResultStart(0));
+    }
+  }
 
   return (
     <>
