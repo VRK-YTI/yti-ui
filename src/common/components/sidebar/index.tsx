c2702c644f856f32dd80a6f9722dee895a0012bb
Jukka Hyytiälä <jukka.hyytiala@gofore.com>
2021-12-14 13:22:29 +0200

    Add reusable Sidebar component

---
+++ src/common/components/sidebar/index.tsx
@@ -0,0 +1,17 @@
+import Sidebar from './sidebar';
+import SidebarLinkListItem from './sidebar-link-list-item';
+import {
+  SidebarHeader,
+  SidebarSubHeader,
+  SidebarDivider,
+  SidebarLinkList,
+} from './sidebar.styles';
+
+export {
+  Sidebar,
+  SidebarHeader,
+  SidebarSubHeader,
+  SidebarDivider,
+  SidebarLinkList,
+  SidebarLinkListItem,
+};
