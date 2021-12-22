931eb8522452643130165254bcd94f340841637c
topirissanen-atoz <topi.rissanen@atoz.fi>
2021-12-22 14:35:28 +0200

    Added mobile filter to vocab-page

--- src/modules/vocabulary/index.tsx
+++ src/modules/vocabulary/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { initializeVocabularyFilter, resetVocabularyFilter, setVocabularyFilter, useGetConceptResultQuery, useGetVocabularyQuery, VocabularyState } from '../../common/components/vocabulary/vocabulary-slice';
 import Filter from '../../common/components/filter/filter';
 import SearchResults from '../../common/components/search-results/search-results';
@@ -7,12 +7,18 @@ import { ResultAndFilterContainer, ResultAndStatsWrapper } from './vocabulary.st
 import { selectVocabularyFilter } from '../../common/components/vocabulary/vocabulary-slice';
 import { useSelector } from 'react-redux';
 import { useStoreDispatch } from '../../store';
+import { useBreakpoints } from '../../common/components/media-query/media-query-context';
+import { FilterMobileButton } from '../terminology-search/terminology-search.styles';
+import { useTranslation } from 'next-i18next';
+import { Modal, ModalContent } from 'suomifi-ui-components';
 
 interface VocabularyProps {
   id: string;
 }
 
 export default function Vocabulary({ id }: VocabularyProps) {
+  const { t } = useTranslation('common');
+  const { isSmall } = useBreakpoints();
   const dispatch = useStoreDispatch();
   useEffect(() => {
     dispatch(initializeVocabularyFilter());
@@ -21,10 +27,20 @@ export default function Vocabulary({ id }: VocabularyProps) {
   const filter: VocabularyState['filter'] = useSelector(selectVocabularyFilter());
   const { data: concepts } = useGetConceptResultQuery(id);
   const { data: info } = useGetVocabularyQuery(id);
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
       {info && <Title info={info} />}
+      {isSmall &&
+        <FilterMobileButton
+          variant='secondary'
+          fullWidth
+          onClick={() => setShowModal(!showModal)}
+        >
+          {t('vocabulary-filter-filter-list')}
+        </FilterMobileButton>
+      }
       <ResultAndFilterContainer>
         {concepts &&
           <ResultAndStatsWrapper>
@@ -35,12 +51,37 @@ export default function Vocabulary({ id }: VocabularyProps) {
             />
           </ResultAndStatsWrapper>
         }
-        <Filter
-          filter={filter as VocabularyState['filter']}
-          type={'vocabulary'}
-          setSomeFilter={setVocabularyFilter}
-          resetSomeFilter={resetVocabularyFilter}
-        />
+        {!isSmall
+          ?
+          <Filter
+            filter={filter as VocabularyState['filter']}
+            type={'vocabulary'}
+            setSomeFilter={setVocabularyFilter}
+            resetSomeFilter={resetVocabularyFilter}
+          />
+          :
+          <Modal
+            appElementId='__next'
+            visible={showModal}
+            onEscKeyDown={() => setShowModal(false)}
+            variant='smallScreen'
+            style={{ border: 'none' }}
+          >
+            <ModalContent
+              style={{ padding: '0' }}
+            >
+              <Filter
+                filter={filter as VocabularyState['filter']}
+                type={'vocabulary'}
+                setSomeFilter={setVocabularyFilter}
+                resetSomeFilter={resetVocabularyFilter}
+                isModal={true}
+                setShowModal={setShowModal}
+                resultCount={concepts?.totalHitCount}
+              />
+            </ModalContent>
+          </Modal>
+        }
       </ResultAndFilterContainer>
     </>
   );
