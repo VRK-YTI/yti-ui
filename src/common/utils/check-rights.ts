7a3e5e426f9d1878ea6714d8b8562385a84ab70e
topirissanen-atoz <topi.rissanen@atoz.fi>
2021-12-31 07:33:02 +0200

    Added small improvements

--- src/common/utils/check-rights.ts
+++ src/common/utils/check-rights.ts
@@ -1,12 +1,19 @@
 import User from '../interfaces/user-interface';
 import { VocabularyInfoDTO } from '../interfaces/vocabulary.interface';
 
-export default function checkRights(loginInfo: User, data: VocabularyInfoDTO) {
+const editRoles = [
+  'ADMIN'
+];
+
+export default function hasRights(user: User, target?: VocabularyInfoDTO) {
   let hasRights = false;
 
-  data?.references.contributor?.forEach(c => {
-    Object.keys(loginInfo.rolesInOrganizations).forEach(k => {
-      if (c.id === k && loginInfo.rolesInOrganizations[k].includes('ADMIN')) {
+  target?.references.contributor?.forEach(c => {
+    Object.keys(user.rolesInOrganizations).forEach(k => {
+      if (
+        c.id === k &&
+        user.rolesInOrganizations[k].some(role => editRoles.includes(role))
+      ) {
         hasRights = true;
       }
     });
