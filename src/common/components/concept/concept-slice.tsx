f40b5999138a7bd1cf077e42cc41f62ef0a980af
topirissanen-atoz <topi.rissanen@atoz.fi>
2022-01-04 11:32:00 +0200

    Switched from Fetch to Axios

--- src/common/components/concept/concept-slice.tsx
+++ src/common/components/concept/concept-slice.tsx
@@ -1,18 +1,16 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
 import { Concept } from '../../interfaces/concept.interface';
+import axiosBaseQuery from '../axios-base-query';
 
 export const conceptApi = createApi({
   reducerPath: 'conceptAPI',
-  baseQuery: fetchBaseQuery({ baseUrl: '/terminology-api/api/v1/frontend' }),
+  baseQuery: axiosBaseQuery({ baseUrl: '/terminology-api/api/v1/frontend' }),
   tagTypes: ['Concept'],
   endpoints: builder => ({
     getConcept: builder.query<Concept, { terminologyId: string, conceptId: string }>({
       query: ({ terminologyId, conceptId }) => ({
         url: `/concept?graphId=${terminologyId}&conceptId=${conceptId}`,
-        method: 'GET',
-        headers: {
-          'content-type': 'application/json',
-        },
+        method: 'GET'
       })
     })
   }),
