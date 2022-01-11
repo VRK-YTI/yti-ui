a10ef01d4081adefa716c73fabd194b07b10b451
Jukka Hyytiälä <jukka.hyytiala@gofore.com>
2022-01-11 13:20:02 +0200

    Use dayjs when formatting dates

--- src/common/components/formatted-date/index.tsx
+++ src/common/components/formatted-date/index.tsx
@@ -1,4 +1,6 @@
 import { useTranslation } from 'next-i18next';
+import { Locale } from '../locale-chooser/use-locales';
+import format from './format';
 
 export interface FormattedDateProps {
   date?: string;
@@ -8,40 +10,10 @@ export default function FormattedDate({ date }: FormattedDateProps) {
   const { i18n } = useTranslation('common');
 
   if (!date) {
-    return <></>;
+    return null;
   }
 
   return (
-    <>{formatISODate(date, i18n.language)}</>
+    <>{format(date, i18n.language as Locale)}</>
   );
-}
-
-export function formatISODate(ISODate: string | undefined, locale: string = 'fi') {
-  if (ISODate) {
-    let date = '';
-    let hour = '';
-
-    switch (locale) {
-    case 'fi':
-      date = ISODate.split('T')[0].split('-').reverse().join('.');
-      hour = ISODate.split('T')[1].split('.')[0].split(':').slice(0, 2).join('.');
-
-      return date + ', ' + hour;
-    case 'en':
-      let dateArr = ISODate.split('T')[0].split('-').reverse();
-      dateArr = [dateArr[1], dateArr[0], dateArr[2]];
-      date = dateArr.join('/');
-
-      hour = ISODate.split('T')[1].split('.')[0].split(':').slice(0, 2).join('.');
-
-      return date + ', ' + hour;
-    case 'sv':
-      date = ISODate.split('T')[0].split('-').reverse().join('.');
-      hour = ISODate.split('T')[1].split('.')[0].split(':').slice(0, 2).join('.');
-
-      return date + ' kl. ' + hour;
-    default:
-      return '';
-    }
-  }
 }
