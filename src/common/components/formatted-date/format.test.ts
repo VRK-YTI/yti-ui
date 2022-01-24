9f1fe566a04d68a9c743db3a28e4f7ab1eab2f8d
Jukka Hyytiälä <jukka.hyytiala@gofore.com>
2022-01-24 11:09:09 +0200

    Small fixes (#55)
    - Use dayjs when formatting dates + fix date formats.
    - Fix some other minor bugs.
    
    YTI-1629

---
+++ src/common/components/formatted-date/format.test.ts
@@ -0,0 +1,19 @@
+import format from './format';
+
+describe('format', () => {
+  it('should format date correctly in Finnish', async () => {
+    expect(format('2022-01-02T03:04:00.000Z', 'fi')).toEqual('2.1.2022, 5.04');
+  });
+
+  it('should format date correctly in Swedish', async () => {
+    expect(format('2022-01-02T03:04:00.000Z', 'sv')).toEqual('2.1.2022 kl. 5.04');
+  });
+
+  it('should format date correctly in English', async () => {
+    expect(format('2022-01-02T03:04:00.000Z', 'en')).toEqual('01/02/2022, 5.04');
+  });
+
+  it('should use 24 hour clock in English', async () => {
+    expect(format('2022-01-02T12:04:00.000Z', 'en')).toEqual('01/02/2022, 14.04');
+  });
+});
---
+++ src/common/components/formatted-date/formatted-date.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FormattedDate from '.';
+
+describe('FormattedDate', () => {
+  it('should render date', async () => {
+    render(
+      <FormattedDate date="2022-01-02T03:04:00.000Z" />
+    );
+
+    expect(screen.queryByText('2.1.2022, 5.04')).toBeTruthy();
+  });
+});
