89004952b4413c8b010947d9b521f3686f0873ab
topirissanen-atoz <topi.rissanen@atoz.fi>
2022-02-07 11:43:22 +0200

    Sidebar translation works for concepts

--- src/common/components/sidebar/sidebar-section.tsx
+++ src/common/components/sidebar/sidebar-section.tsx
@@ -4,13 +4,14 @@ import { Link as SuomiLink } from 'suomifi-ui-components';
 import { SidebarLinkList, SidebarLinkListItem, SidebarSubHeader } from '.';
 import { BaseEntity, Property } from '../../interfaces/termed-data-types.interface';
 import PropertyValue from '../property-value';
-import { getPropertyValue } from '../property-value/get-property-value';
+import { Term } from '../../interfaces/term.interface';
+import { isEmpty } from 'lodash';
 
 interface SidebarSectionProps<T> {
   heading: React.ReactNode;
   items?: T[];
   href: (item: T) => string;
-  propertyAccessor: (item: T) => Property[] | undefined;
+  propertyAccessor: (item: T) => Property[] | Term[] | undefined;
 }
 
 export default function SidebarSection<T extends BaseEntity<string>>({
@@ -23,13 +24,6 @@ export default function SidebarSection<T extends BaseEntity<string>>({
     return null;
   }
 
-  const test = {property: Array.from(propertyAccessor(items[0]), x => x.properties?.prefLabel[0])};
-  console.log(test);
-
-  if (test.property[0]) {
-    getPropertyValue({property: test, language: 'en'});
-  }
-
   return (
     <>
       <SidebarSubHeader>{heading}</SidebarSubHeader>
@@ -38,10 +32,7 @@ export default function SidebarSection<T extends BaseEntity<string>>({
           <SidebarLinkListItem key={item.id}>
             <Link href={href(item)} passHref>
               <SuomiLink href=''>
-                <PropertyValue
-                  property={propertyAccessor(item)}
-                  fallbackLanguage="fi"
-                />
+                {propertyValue(item)}
               </SuomiLink>
             </Link>
           </SidebarLinkListItem>
@@ -49,4 +40,28 @@ export default function SidebarSection<T extends BaseEntity<string>>({
       </SidebarLinkList>
     </>
   );
+
+  function propertyValue(currItem: T) {
+    if (!isEmpty(currItem.references) && currItem !== undefined) {
+      const prefLabels = Array.from(propertyAccessor(currItem) as Term[], x => {
+        if (x.properties?.prefLabel && x.properties.prefLabel[0]) {
+          return x.properties.prefLabel[0];
+        }
+      });
+
+      return (
+        <PropertyValue
+          property={prefLabels as Property[]}
+          fallbackLanguage='fi'
+        />
+      );
+    } else {
+      return (
+        <PropertyValue
+          property={propertyAccessor(currItem) as Property[]}
+          fallbackLanguage='fi'
+        />
+      );
+    }
+  }
 }
