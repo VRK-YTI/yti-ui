5980402211f07a49802c8513d5fb94135d9f9962
topirissanen-atoz <topi.rissanen@atoz.fi>
2022-03-07 07:06:42 +0200

    Screen reader support for logo added

--- src/modules/smart-header/logo.tsx
+++ src/modules/smart-header/logo.tsx
@@ -3,15 +3,17 @@ import { Link as SuomiLink } from 'suomifi-ui-components';
 import Image from 'next/image';
 import Link from 'next/link';
 import { LogoWrapper } from './smart-header.styles';
+import { useTranslation } from 'next-i18next';
 
 export default function Logo() {
+  const { t } = useTranslation('common');
   return (
     <LogoWrapper>
       <Link href="/" passHref>
-        <SuomiLink href=''>
-          <Image src='/logo.svg' width="186" height="32" alt="Logo" />
+        <SuomiLink href='' aria-label={t('navigate-to-homepage')}>
+          <Image src='/logo.svg' width="186" height="32" alt="" title='Siirry etusivulle' />
         </SuomiLink>
       </Link>
-    </LogoWrapper>
+    </LogoWrapper >
   );
 }
