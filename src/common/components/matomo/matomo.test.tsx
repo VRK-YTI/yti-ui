8bd0b214c2bd3b2742b3e4ae18bbe20ff324e96f
Jukka Hyytiälä <jukka.hyytiala@gofore.com>
2022-04-14 13:25:28 +0300

    Add more tests

--- src/common/components/matomo/matomo.test.tsx
+++ src/common/components/matomo/matomo.test.tsx
@@ -1,51 +1,104 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { useRouter } from 'next/router';
+import { render, act } from '@testing-library/react';
 import Matomo, { MatomoTracking } from '.';
+import mockRouter from 'next-router-mock';
+import singletonRouter from 'next/router';
 
-jest.mock('next/router');
-const mockedUseRouter = useRouter as jest.MockedFunction<typeof useRouter>;
+jest.useFakeTimers();
+jest.mock('next/dist/client/router', () => require('next-router-mock'));
 
 describe('matomo', () => {
+  beforeEach(() => {
+    mockRouter.setCurrentUrl('/');
+    delete window._paq;
+  });
   it('should not render in test when used smart version', () => {
-    mockedUseRouter.mockReturnValue({
-      asPath: '/',
-      events: {
-        on: jest.fn(),
-        off: jest.fn(),
-      },
-    } as any); // eslint-disable-line @typescript-eslint/no-explicit-any
-
     render(<Matomo />);
 
     expect(window._paq).toBeUndefined();
   });
 
   it('should render when used dummy version', () => {
-    mockedUseRouter.mockReturnValue({
-      asPath: '/',
-      events: {
-        on: jest.fn(),
-        off: jest.fn(),
-      },
-    } as any); // eslint-disable-line @typescript-eslint/no-explicit-any
-
     render(<MatomoTracking url="matamo-test-url" siteId="1234" />);
 
     expect(window._paq).toBeDefined();
   });
 
   it('should use cookieless tracking', () => {
-    mockedUseRouter.mockReturnValue({
-      asPath: '/',
-      events: {
-        on: jest.fn(),
-        off: jest.fn(),
-      },
-    } as any); // eslint-disable-line @typescript-eslint/no-explicit-any
-
     render(<MatomoTracking url="matamo-test-url" siteId="1234" />);
 
-    expect(window._paq[0]).toStrictEqual(['disableCookies']);
+    expect(window._paq?.[0]).toStrictEqual(['disableCookies']);
+  });
+
+  it('should use correct url', () => {
+    render(<MatomoTracking url="matamo-test-url" siteId="1234" />);
+
+    expect(window._paq?.[3]).toStrictEqual([
+      'setTrackerUrl',
+      'matamo-test-url/matomo.php',
+    ]);
+  });
+
+  it('should use correct siteId', () => {
+    render(<MatomoTracking url="matamo-test-url" siteId="1234" />);
+
+    expect(window._paq?.[4]).toStrictEqual(['setSiteId', '1234']);
+  });
+
+  it('should track initial page view', () => {
+    render(<MatomoTracking url="matamo-test-url" siteId="1234" />);
+
+    expect(window._paq?.[1]).toStrictEqual(['trackPageView']);
+  });
+
+  it('should track page changes', () => {
+    render(<MatomoTracking url="matamo-test-url" siteId="1234" />);
+    act(() => {
+      singletonRouter.push('/another-page');
+      document.title = 'Another Page';
+    });
+
+    jest.runAllTimers();
+
+    expect(window._paq?.[6]).toStrictEqual(['setCustomUrl', '/another-page']);
+    expect(window._paq?.[8]).toStrictEqual(['trackPageView']);
+  });
+
+  it('should log referrer url when page changes', () => {
+    render(<MatomoTracking url="matamo-test-url" siteId="1234" />);
+    act(() => {
+      singletonRouter.push('/another-page');
+      document.title = 'Another Page';
+    });
+
+    jest.runAllTimers();
+
+    expect(window._paq?.[5]).toStrictEqual(['setReferrerUrl', '/']);
+  });
+
+  it('should log new page title when page changes', () => {
+    render(<MatomoTracking url="matamo-test-url" siteId="1234" />);
+    act(() => {
+      singletonRouter.push('/another-page');
+      document.title = 'Another Page';
+    });
+
+    jest.runAllTimers();
+
+    expect(window._paq?.[7]).toStrictEqual([
+      'setDocumentTitle',
+      'Another Page',
+    ]);
+  });
+
+  it('should ignore changes in query parameters', () => {
+    render(<MatomoTracking url="matamo-test-url" siteId="1234" />);
+    act(() => {
+      singletonRouter.push('/?q=qwerty');
+    });
+
+    jest.runAllTimers();
+
+    expect(window._paq).toHaveLength(5);
   });
 });
