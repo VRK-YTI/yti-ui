47a11d8ce2e1052ec6f34b4f4832d4e465cb860a
Jukka Hyytiälä <jukka.hyytiala@gofore.com>
2022-04-19 13:57:31 +0300

    Use SingleSelect in organization filter
    Replace Dropdown with new SingleSelect component in organization
    filter. The main difference between them are that SingleSelect
    has an autocomplete feature.
    
    YTI-1640

--- src/common/components/filter/organization-filter.tsx
+++ src/common/components/filter/organization-filter.tsx
@@ -1,9 +1,10 @@
-import { Dropdown, DropdownItem } from 'suomifi-ui-components';
+import { SingleSelect } from 'suomifi-ui-components';
 import { OrganizationSearchResult } from '@app/common/interfaces/terminology.interface';
 import useUrlState, {
   initialUrlState,
 } from '@app/common/utils/hooks/useUrlState';
-import { DropdownPlaceholder, DropdownWrapper } from './filter.styles';
+import { DropdownWrapper } from './filter.styles';
+import { useTranslation } from 'next-i18next';
 
 interface OrganizationFilterProps {
   organizations?: OrganizationSearchResult[];
@@ -17,32 +18,39 @@ export default function OrganizationFilter({
   visualPlaceholder,
 }: OrganizationFilterProps) {
   const { urlState, patchUrlState } = useUrlState();
+  const { t } = useTranslation();
 
   if (!organizations) {
     return null;
   }
 
+  const items = organizations.map((organization) => ({
+    labelText: organization.properties.prefLabel.value,
+    uniqueItemId: organization.id,
+  }));
+
+  const selectedItem = items.find(
+    (item) => item.uniqueItemId === urlState.organization
+  );
   return (
     <DropdownWrapper>
-      <Dropdown
+      <SingleSelect
         labelText={title}
-        visualPlaceholder={
-          <DropdownPlaceholder>{visualPlaceholder}</DropdownPlaceholder>
+        clearButtonLabel={t('terminology-search-filter-clear-organization')}
+        items={items}
+        selectedItem={selectedItem}
+        visualPlaceholder={visualPlaceholder}
+        noItemsText={t('terminology-search-filter-no-matching-organizations')}
+        ariaOptionsAvailableText={
+          'terminology-search-filter-organizations-available'
         }
-        value={urlState.organization}
-        onChange={(organization) =>
+        onItemSelect={(uniqueItemId) =>
           patchUrlState({
-            organization,
+            organization: uniqueItemId ?? undefined,
             page: initialUrlState.page,
           })
         }
-      >
-        {organizations.map((organization) => (
-          <DropdownItem value={organization.id} key={organization.id}>
-            {organization.properties.prefLabel.value}
-          </DropdownItem>
-        ))}
-      </Dropdown>
+      />
     </DropdownWrapper>
   );
 }
