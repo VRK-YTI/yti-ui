54befc29d8711d9d41dcc685d1eb0afe64e4e488
Jukka Hyytiälä <jukka.hyytiala@gofore.com>
2022-05-04 08:22:20 +0300

    New concept page heading (#142)
    Add page heading to new concept page
    
    YTI-1848

--- src/modules/new-concept/index.tsx
+++ src/modules/new-concept/index.tsx
@@ -1,10 +1,18 @@
 import { Breadcrumb, BreadcrumbLink } from '@app/common/components/breadcrumb';
 import PropertyValue from '@app/common/components/property-value';
 import { useGetVocabularyQuery } from '@app/common/components/vocabulary/vocabulary.slice';
+import { getProperty } from '@app/common/utils/get-property';
+import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
-import { Heading } from 'suomifi-ui-components';
+import { useRef } from 'react';
+import { Heading, Text } from 'suomifi-ui-components';
 import ConceptBasicInformation from './basic-information/concept-basic-information';
-import { NewConceptBlock } from './new-concept.styles';
+import {
+  Badge,
+  BadgeBar,
+  HeadingBlock,
+  NewConceptBlock,
+} from './new-concept.styles';
 
 interface NewConceptProps {
   terminologyId: string;
@@ -15,8 +23,10 @@ export default function NewConcept({
   terminologyId,
   conceptNames,
 }: NewConceptProps) {
+  const { t } = useTranslation('concept');
   const router = useRouter();
   const { data: terminology } = useGetVocabularyQuery(terminologyId);
+  const titleRef = useRef<HTMLHeadingElement>(null);
 
   return (
     <>
@@ -37,7 +47,31 @@ export default function NewConcept({
       </Breadcrumb>
 
       <NewConceptBlock>
-        <Heading variant="h1">{getTermName()}</Heading>
+        <HeadingBlock>
+          <Text>
+            <PropertyValue
+              property={getProperty(
+                'prefLabel',
+                terminology?.references.contributor
+              )}
+              fallbackLanguage="fi"
+            />
+          </Text>
+          <Heading variant="h1" tabIndex={-1} ref={titleRef}>
+            {getTermName()}
+          </Heading>
+          <BadgeBar>
+            <span>{t('heading')}</span> &middot;{' '}
+            <span>
+              <PropertyValue
+                property={terminology?.properties.prefLabel}
+                fallbackLanguage="fi"
+              />
+            </span>{' '}
+            &middot; <Badge>{t('DRAFT')}</Badge>
+          </BadgeBar>
+          <Text>{t('new-concept-page-help')}</Text>
+        </HeadingBlock>
 
         <ConceptBasicInformation />
       </NewConceptBlock>
