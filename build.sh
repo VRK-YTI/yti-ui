943c034c84614e8e020969f72916979a4df248e4
Jukka Hyytiälä <jukka.hyytiala@gofore.com>
2022-05-23 08:18:55 +0300

    Add SKIP_COVERAGE build argument (#149)
    Add SKIP_COVERAGE build argument. If it is passed (e.g. via environment
    variables), the coverage directory is excluded from the built image. The idea
    is to not bloat the production image with development-related stuff.
    
    YTI-1890

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ docker build \
   -t yti-terminology-ui \
   --build-arg REWRITE_PROFILE=${REWRITE_PROFILE:=docker} \
   --build-arg SKIP_TESTS=${SKIP_TESTS:=} \
+  --build-arg SKIP_COVERAGE=${SKIP_COVERAGE:=} \
   .
