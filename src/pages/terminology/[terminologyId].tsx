4798a5e65c95c3b82d38af3e9dc889527b1ab16d
Kalle Kyttälä <kalle.kyttala@rebase.fi>
2022-05-24 12:16:32 +0300

    fix linter errors

--- src/pages/terminology/[terminologyId].tsx
+++ src/pages/terminology/[terminologyId].tsx
@@ -75,7 +75,9 @@ export const getServerSideProps = createCommonGetServerSideProps(
 
     await store.dispatch(getVocabulary.initiate(id));
     await store.dispatch(getCollections.initiate(id));
-    await store.dispatch(getConceptResult.initiate({ urlState: urlState, id, language: locale }));
+    await store.dispatch(
+      getConceptResult.initiate({ urlState: urlState, id, language: locale })
+    );
 
     await Promise.all(getRunningOperationPromises());
 
