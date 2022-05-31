783c09e9115e52c1929410b3fa4b6e607ef32806
topirissanen-atoz <topi.rissanen@atoz.fi>
2022-05-31 13:11:35 +0300

    SSR functionality added to index page

--- src/common/components/counts/counts.slice.tsx
+++ src/common/components/counts/counts.slice.tsx
@@ -1,10 +1,20 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { Counts } from '@app/common/interfaces/counts.interface';
 import axiosBaseQuery from '@app/common/components/axios-base-query';
+import { HYDRATE } from 'next-redux-wrapper';
 
 export const countsApi = createApi({
   reducerPath: 'countsApi',
-  baseQuery: axiosBaseQuery({ baseUrl: '/terminology-api/api/v1/frontend' }),
+  baseQuery: axiosBaseQuery({
+    baseUrl: process.env.TERMINOLOGY_API_URL
+      ? `${process.env.TERMINOLOGY_API_URL}/api/v1/frontend`
+      : '/terminology-api/api/v1/frontend'
+  }),
+  extractRehydrationInfo(action, { reducerPath }) {
+    if (action.type === HYDRATE) {
+      return action.payload[reducerPath];
+    }
+  },
   tagTypes: ['Counts'],
   endpoints: (builder) => ({
     getCounts: builder.query<Counts, null>({
@@ -25,4 +35,10 @@ export const countsApi = createApi({
   }),
 });
 
-export const { useGetCountsQuery, useGetVocabularyCountQuery } = countsApi;
+export const {
+  useGetCountsQuery,
+  useGetVocabularyCountQuery,
+  util: { getRunningOperationPromises },
+} = countsApi;
+
+export const { getCounts, getVocabularyCount } = countsApi.endpoints;
