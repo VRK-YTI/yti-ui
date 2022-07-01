8a15dee8ce834e66dcc41e4feb8c7b586197de19
topirissanen-atoz <topi.rissanen@atoz.fi>
2022-07-01 09:06:23 +0300

    Added passHref and fixed lint errors

--- src/common/components/error/error.styles.tsx
+++ src/common/components/error/error.styles.tsx
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
 import { Block, Heading, Link, StaticIcon } from 'suomifi-ui-components';
 
-export const ErrorCard = styled(Block)<{$isSmall: boolean}>`
-  align-items: ${props => props.$isSmall === true ? 'center' : 'flex-start'};
-  background: ${props => props.theme.suomifi.colors.whiteBase};
-  border: 1px solid ${props => props.theme.suomifi.colors.depthLight1};
+export const ErrorCard = styled(Block)<{ $isSmall: boolean }>`
+  align-items: ${(props) =>
+    props.$isSmall === true ? 'center' : 'flex-start'};
+  background: ${(props) => props.theme.suomifi.colors.whiteBase};
+  border: 1px solid ${(props) => props.theme.suomifi.colors.depthLight1};
   display: flex;
-  flex-direction: ${props => props.$isSmall === true ? 'column' : 'row'};
+  flex-direction: ${(props) => (props.$isSmall === true ? 'column' : 'row')};
   gap: 25px;
-  margin-bottom: ${props => props.theme.suomifi.spacing.m};
+  margin-bottom: ${(props) => props.theme.suomifi.spacing.m};
   margin-top: 20px;
-  padding: ${props => props.$isSmall === true ? '25px 35px' : '25px 75px 35px 30px'};
+  padding: ${(props) =>
+    props.$isSmall === true ? '25px 35px' : '25px 75px 35px 30px'};
   width: 1110px;
 `;
 
--- src/common/components/error/error.tsx
+++ src/common/components/error/error.tsx
@@ -1,6 +1,12 @@
 import { Paragraph, Text } from 'suomifi-ui-components';
 import { useBreakpoints } from '../media-query/media-query-context';
-import { ErrorCard, ErrorIcon, HomePageLink, ParagraphTitle, TextBlock } from './error.styles';
+import {
+  ErrorCard,
+  ErrorIcon,
+  HomePageLink,
+  ParagraphTitle,
+  TextBlock,
+} from './error.styles';
 import Link from 'next/link';
 
 export interface ErrorProps {
@@ -12,44 +18,50 @@ export default function Error({ errorCode }: ErrorProps) {
 
   return (
     <ErrorCard $isSmall={isSmall}>
-      <ErrorIcon icon='failure' />
+      <ErrorIcon icon="failure" />
       <TextBlock>
         <div>
-          <ParagraphTitle variant='h2'>Sivua ei löytynyt</ParagraphTitle>
+          <ParagraphTitle variant="h2">Sivua ei löytynyt</ParagraphTitle>
           <Paragraph>
             <Text smallScreen>
-              Etsimääsi sivua ei valitettavasti löytynyt. Tätä sivua ei ole saatavilla tai sivun osoite on muuttunut. Tarkistathan, että kirjoittamasi osoite on oikein.
+              Etsimääsi sivua ei valitettavasti löytynyt. Tätä sivua ei ole
+              saatavilla tai sivun osoite on muuttunut. Tarkistathan, että
+              kirjoittamasi osoite on oikein.
             </Text>
           </Paragraph>
           <Paragraph>
-            <Link href='/'>
-              <HomePageLink href=''>Siirry etusivulle</HomePageLink>
+            <Link href="/" passHref>
+              <HomePageLink href="">Siirry etusivulle</HomePageLink>
             </Link>
           </Paragraph>
         </div>
         <div>
-          <ParagraphTitle variant='h2'>Sidan kunde inte hittas</ParagraphTitle>
+          <ParagraphTitle variant="h2">Sidan kunde inte hittas</ParagraphTitle>
           <Paragraph>
             <Text smallScreen>
-              Sidan som du sökte kunde tyvärr inte hittas. Den här sidan finns inte eller sidan har flyttats. Vänligen kontrollera att adressen är korrekt.
+              Sidan som du sökte kunde tyvärr inte hittas. Den här sidan finns
+              inte eller sidan har flyttats. Vänligen kontrollera att adressen
+              är korrekt.
             </Text>
           </Paragraph>
           <Paragraph>
-            <Link href='/'>
-              <HomePageLink href=''>Gå till startsidan</HomePageLink>
+            <Link href="/" passHref>
+              <HomePageLink href="">Gå till startsidan</HomePageLink>
             </Link>
           </Paragraph>
         </div>
         <div>
-          <ParagraphTitle variant='h2'>Page was not found</ParagraphTitle>
+          <ParagraphTitle variant="h2">Page was not found</ParagraphTitle>
           <Paragraph>
             <Text smallScreen>
-              The page you were looking for could not be found. This page is not available or the page has been moved. Please make sure that the address is spelled correctly.
+              The page you were looking for could not be found. This page is not
+              available or the page has been moved. Please make sure that the
+              address is spelled correctly.
             </Text>
           </Paragraph>
           <Paragraph>
-            <Link href='/'>
-              <HomePageLink href=''>Go to the home page</HomePageLink>
+            <Link href="/" passHref>
+              <HomePageLink href="">Go to the home page</HomePageLink>
             </Link>
           </Paragraph>
         </div>
