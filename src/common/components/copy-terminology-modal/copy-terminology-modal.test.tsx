d20e40cecd327b04785a745800c038322d8883bd
Tomi Matsumoto-Viljanen <tomi.matsumoto-viljanen@gofore.com>
2022-08-10 14:52:14 +0300

    matchMedia not supported by jest. mock matchmedia

--- src/common/components/copy-terminology-modal/copy-terminology-modal.test.tsx
+++ src/common/components/copy-terminology-modal/copy-terminology-modal.test.tsx
@@ -11,6 +11,19 @@ describe('render', () => {
     appRoot = document.createElement('div');
     appRoot.setAttribute('id', '__next');
     document.body.appendChild(appRoot);
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
   });
 
   it('should render component', () => {
