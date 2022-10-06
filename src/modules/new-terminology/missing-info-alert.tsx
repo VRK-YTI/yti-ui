255390c53e16131fd979e44d593e0bc7727541b1
topirissanen-atoz <topi.rissanen@atoz.fi>
2022-10-06 09:29:09 +0300

    Removed unused code

--- src/modules/new-terminology/missing-info-alert.tsx
+++ src/modules/new-terminology/missing-info-alert.tsx
@@ -2,8 +2,6 @@ import FormFooterAlert from '@app/common/components/form-footer-alert';
 import { NewTerminologyInfo } from '@app/common/interfaces/new-terminology-info';
 import { translateLanguage } from '@app/common/utils/translation-helpers';
 import { useTranslation } from 'next-i18next';
-import { InlineAlert } from 'suomifi-ui-components';
-import { MissingInfoAlertUl } from './new-terminology.styles';
 
 interface MissingInfoAlertProps {
   data: NewTerminologyInfo;
@@ -27,19 +25,6 @@ export default function MissingInfoAlert({ data }: MissingInfoAlertProps) {
           renderStatusAlerts(),
         ].filter((alert) => alert)}
       />
-    );
-
-    return (
-      <InlineAlert status="warning" role="alert" id="missing-info-alert">
-        {t('alert-missing-information')}:
-        <MissingInfoAlertUl>
-          {renderDescriptionAlerts()}
-          {renderOrganizationAlerts()}
-          {renderInformationDomainAlerts()}
-          {renderPrefixAlerts()}
-          {renderStatusAlerts()}
-        </MissingInfoAlertUl>
-      </InlineAlert>
     );
   } else {
     return <></>;
@@ -114,60 +99,4 @@ export default function MissingInfoAlert({ data }: MissingInfoAlertProps) {
 
     return '';
   }
-
-  // function renderDescriptionAlerts() {
-  //   return (
-  //     <>
-  //       {data.description[0].length === 0 && (
-  //         <li>{t('alert-description-languages-undefined')}</li>
-  //       )}
-  //       {data.description[0]
-  //         .filter((desc) => !desc.name)
-  //         .map((desc, idx) => {
-  //           return (
-  //             <li key={`description-${idx}`}>
-  //               {t('alert-description-name-undefined')}{' '}
-  //               {translateLanguage(desc.lang, t)}
-  //             </li>
-  //           );
-  //         })}
-  //     </>
-  //   );
-  // }
-
-  // function renderOrganizationAlerts() {
-  //   return (
-  //     <>
-  //       {data.contributors.length === 0 && <li>{t('alert-org-undefined')}</li>}
-  //     </>
-  //   );
-  // }
-
-  // function renderInformationDomainAlerts() {
-  //   return (
-  //     <>
-  //       {data.infoDomains.length === 0 && (
-  //         <li>{t('alert-information-domain-undefined')}</li>
-  //       )}
-  //     </>
-  //   );
-  // }
-
-  // function renderPrefixAlerts() {
-  //   if (!data.prefix[0]) {
-  //     return <li>{t('alert-prefix-undefined')}</li>;
-  //   }
-
-  //   if (data.prefix[1] === false) {
-  //     return <li>{t('alert-prefix-invalid-or-in-use')}</li>;
-  //   }
-
-  //   return <></>;
-  // }
-
-  // function renderStatusAlerts() {
-  //   if (Object.keys(data).includes('status') && !data.status) {
-  //     return <li>{t('alert-no-status')}</li>;
-  //   }
-  // }
 }
