6013664bbf3bc4b09742e118a3c30e9828252708
Kalle Kyttälä <kalle.kyttala@rebase.fi>
2022-10-27 12:00:00 +0300

    test build failure

--- datamodel-ui/pages/index.tsx
+++ datamodel-ui/pages/index.tsx
@@ -1,4 +1,4 @@
-// import SaveSpinner from '@common/components/save-spinner';
+import SaveSpinner from '@common/components/save-spinner';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
