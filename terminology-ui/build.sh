b077aaf3706ed6612cb32407708074c5fc4ac6df
Tomi Matsumoto-Viljanen <19401683+tomiviljanen@users.noreply.github.com>
2022-11-02 10:34:48 +0200

    YTI-2113: Monorepo into v2 (#304)
    Move changes from monorepo branch to v2

--- terminology-ui/build.sh
+++ terminology-ui/build.sh
@@ -2,7 +2,8 @@
 
 docker build \
   -t yti-terminology-ui \
+  -f "Dockerfile" \
   --build-arg REWRITE_PROFILE=${REWRITE_PROFILE:=docker} \
   --build-arg SKIP_TESTS=${SKIP_TESTS:=} \
   --build-arg SKIP_COVERAGE=${SKIP_COVERAGE:=} \
-  .
+  ..
