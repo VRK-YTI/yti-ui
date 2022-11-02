c691bbbf096246c1b5b9f10dfdebd7e790baead0
Urho Tamminen <urho.tamminen@gofore.com>
2022-11-02 15:46:31 +0200

    pass Shibboleth related headers to API calls in SSR
    This is to work around an issue where JSESSIONID is sometimes not enough.
    
    In total, the following things are passed:
    
    * JSESSIONID: session cookie for the spring api, first stored in
      iron-session due to a different path
    * _shibsession_...: Shibboleth session cookie
    * Host: Header needs to match the client request or Shibboleth will
      invalidate the session
    * X-Forwarded-For: Same for this one
    
    In case TERMINOLOGY_API_URL is not pointing to Shibboleth, only JSESSIONID matters.

--- terminology-ui/src/pages/api/auth/logout.ts
+++ terminology-ui/src/pages/api/auth/logout.ts
@@ -5,11 +5,16 @@ export default withIronSessionApiRoute(
   async function logout(req, res) {
     const target = (req.query['target'] as string) ?? '/';
 
-    // invalidate cookie
-    res.setHeader(
-      'Set-Cookie',
-      'JSESSIONID=deleted; path=/terminology-api; expires=Thu, 01 Jan 1970 00:00:00 GMT'
-    );
+    // invalidate:
+    // * JSESSIONID
+    // * all Shibboleth session cookies
+    res.setHeader('Set-Cookie', [
+      'JSESSIONID=deleted; path=/terminology-api; expires=Thu, 01 Jan 1970 00:00:00 GMT',
+      ...Object.entries(req.cookies)
+        .filter(([k, _]) => k.startsWith('_shibsession_'))
+        .map(([k, _]) =>
+          `${k}=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`),
+    ]);
 
     req.session.destroy();
     res.redirect(target);
