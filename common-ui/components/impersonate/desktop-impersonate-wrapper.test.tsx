dadfdc9f9d8e4b0aa8946d4caf70d6a88aab7525
topirissanen-atoz <topi.rissanen@atoz.fi>
2022-12-14 11:53:06 +0200

    Fixed common-ui unit tests

--- common-ui/components/impersonate/desktop-impersonate-wrapper.test.tsx
+++ common-ui/components/impersonate/desktop-impersonate-wrapper.test.tsx
@@ -1,69 +1,82 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ImpersonateWrapper from './desktop-impersonate-wrapper';
-import useFakeableUsers, { UseFakeableUsersResult } from './use-fakeable-users';
 import { themeProvider } from '../../utils/test-utils';
-
-jest.mock('./use-fakeable-users');
-const mockedUseFakeableUsers = useFakeableUsers as jest.MockedFunction<
-  typeof useFakeableUsers
->;
 
 describe('impersonateWrapper', () => {
   it('should show "Impersonate user" text', async () => {
-    mockedUseFakeableUsers.mockReturnValue([
+    render(
+      <ImpersonateWrapper
+        fakeableUsers={[
+          {
+            id: '1',
+            email: 'admin@localhost',
+            firstName: 'Admin',
+            lastName: 'User',
+          },
+        ]}
+      >
+        Children
+      </ImpersonateWrapper>,
       {
-        id: '1',
-        email: 'admin@localhost',
-        displayName: 'Admin User',
-      },
-    ] as UseFakeableUsersResult[]);
-
-    render(<ImpersonateWrapper>Children</ImpersonateWrapper>, {
-      wrapper: themeProvider,
-    });
+        wrapper: themeProvider,
+      }
+    );
 
     expect(screen.getByText('tr-impersonate-user')).toBeInTheDocument();
   });
 
   it('should show "Admin User" text', async () => {
-    mockedUseFakeableUsers.mockReturnValue([
+    render(
+      <ImpersonateWrapper
+        fakeableUsers={[
+          {
+            id: '1',
+            email: 'admin@localhost',
+            firstName: 'Admin',
+            lastName: 'User',
+          },
+        ]}
+      >
+        Children
+      </ImpersonateWrapper>,
       {
-        id: '1',
-        email: 'admin@localhost',
-        displayName: 'Admin User',
-      },
-    ] as UseFakeableUsersResult[]);
-
-    render(<ImpersonateWrapper>Children</ImpersonateWrapper>, {
-      wrapper: themeProvider,
-    });
+        wrapper: themeProvider,
+      }
+    );
 
     expect(screen.getByText('Admin User')).toBeInTheDocument();
   });
 
   it('should render children', async () => {
-    mockedUseFakeableUsers.mockReturnValue([
+    render(
+      <ImpersonateWrapper
+        fakeableUsers={[
+          {
+            id: '1',
+            email: 'admin@localhost',
+            firstName: 'Admin',
+            lastName: 'User',
+          },
+        ]}
+      >
+        Children
+      </ImpersonateWrapper>,
       {
-        id: '1',
-        email: 'admin@localhost',
-        displayName: 'Admin User',
-      },
-    ] as UseFakeableUsersResult[]);
-
-    render(<ImpersonateWrapper>Children</ImpersonateWrapper>, {
-      wrapper: themeProvider,
-    });
+        wrapper: themeProvider,
+      }
+    );
 
     expect(screen.getByText('Children')).toBeInTheDocument();
   });
 
   it('should render only children when users were not found', async () => {
-    mockedUseFakeableUsers.mockReturnValue([]);
-
-    render(<ImpersonateWrapper>Children</ImpersonateWrapper>, {
-      wrapper: themeProvider,
-    });
+    render(
+      <ImpersonateWrapper fakeableUsers={[]}>Children</ImpersonateWrapper>,
+      {
+        wrapper: themeProvider,
+      }
+    );
 
     expect(screen.getByText('Children')).toBeInTheDocument();
     expect(screen.queryByText('tr-impersonate-user')).not.toBeInTheDocument();
