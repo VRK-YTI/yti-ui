1e246d6b0b0849ccd6b156690ad017836bd4a8a9
Tomi Matsumoto-Viljanen <19401683+tomiviljanen@users.noreply.github.com>
2023-01-03 07:49:04 +0200

    YTI-2353: Change TEXT_AREA_MAX (#345)
    use common-ui constants instead of defining twice

--- common-ui/utils/constants.ts
+++ common-ui/utils/constants.ts
@@ -1,4 +1,4 @@
-export const TEXT_AREA_MAX = 1500;
+export const TEXT_AREA_MAX = 5000;
 export const TEXT_INPUT_MAX = 150;
 export const EMAIL_MAX = 320;
 export const SEARCH_FIELD_PATTERN = '^(?!.*[<>]).*'; //Allow everything but < and >
--- terminology-ui/src/common/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const TEXT_AREA_MAX = 1500;
-export const TEXT_INPUT_MAX = 150;
-export const EMAIL_MAX = 320;
-export const DEFINITION_MAX = 5000;
-export const SEARCH_FIELD_PATTERN = '^(?!.*[<>]).*'; //Allow everything but < and >
