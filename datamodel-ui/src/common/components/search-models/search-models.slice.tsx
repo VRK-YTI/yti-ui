e4f700d3fcb58835773e145e1605488200deaf38
topirissanen-atoz <topi.rissanen@atoz.fi>
2023-03-15 11:15:45 +0200

    Fixed front page search

--- datamodel-ui/src/common/components/search-models/search-models.slice.tsx
+++ datamodel-ui/src/common/components/search-models/search-models.slice.tsx
@@ -4,6 +4,39 @@ import { getDatamodelApiBaseQuery } from '@app/store/api-base-query';
 import { SearchModels } from '@app/common/interfaces/search-models.interface';
 import { UrlState } from 'yti-common-ui/utils/hooks/use-url-state';
 
+// Drops keys with "empty" values so that only needed
+// values are returned to query parameters
+function getParams(urlState: UrlState, lang?: string) {
+  const filteredParams = Object.fromEntries(
+    Object.entries({
+      pageFrom: Math.max(0, (urlState.page - 1) * 50),
+      pageSize: 50,
+      searchResources: true,
+      sortLang: lang ?? 'fi',
+      query: urlState.q,
+      language: urlState.lang ? urlState.lang : null,
+      organizations: urlState.organization ? [urlState.organization] : [],
+      groups: urlState.domain,
+      type: urlState.types
+        ? urlState.types.map((type) => type.toUpperCase())
+        : [],
+      ...(urlState.status.length === 0
+        ? { status: ['VALID', 'DRAFT'] }
+        : { status: urlState.status }),
+    }).filter(
+      (item) =>
+        item[1] &&
+        item[1] !== null &&
+        item[1] !== undefined &&
+        (Array.isArray(item[1])
+          ? item[1].length > 0
+          : typeof item[1] !== 'undefined')
+    )
+  );
+
+  return filteredParams;
+}
+
 export const searchModelsApi = createApi({
   reducerPath: 'searchModelsApi',
   baseQuery: getDatamodelApiBaseQuery(),
@@ -21,22 +54,7 @@ export const searchModelsApi = createApi({
       query: (props) => ({
         url: '/frontend/searchModels',
         method: 'GET',
-        data: {
-          pageFrom: Math.max(0, (props.urlState.page - 1) * 50),
-          pageSize: 50,
-          searchResources: true,
-          sortLang: props.lang ?? 'fi',
-          query: props.urlState.q,
-          language: props.urlState.lang ? props.urlState.lang : null,
-          organizations: props.urlState.organization
-            ? [props.urlState.organization]
-            : [],
-          groups: props.urlState.domain,
-          type: props.urlState.types ?? [],
-          ...(props.urlState.status.length === 0
-            ? { status: ['VALID', 'DRAFT'] }
-            : { status: props.urlState.status }),
-        },
+        params: getParams(props.urlState, props.lang),
       }),
     }),
   }),
