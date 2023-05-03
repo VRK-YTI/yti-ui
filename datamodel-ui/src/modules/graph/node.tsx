d8b189d275114dbf27a8727ef4c9ccc08bd54f0d
topirissanen-atoz <82932696+topirissanen-atoz@users.noreply.github.com>
2023-05-03 07:23:57 +0300

    Datamodel yti 2565 resource render (#402)
    * Improved edge customization
    
    * Improved edge handling
    
    * Improved edge functionalities

--- datamodel-ui/src/modules/graph/node.tsx
+++ datamodel-ui/src/modules/graph/node.tsx
@@ -1,12 +1,14 @@
 import {
   selectHovered,
   selectSelected,
+  setSelected,
 } from '@app/common/components/model/model.slice';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Handle, Position } from 'reactflow';
 import { Icon } from 'suomifi-ui-components';
 import { Attribute, ClassNodeDiv } from './node.styles';
+import { useStoreDispatch } from '@app/store';
 
 interface ClassNodeProps {
   id: string;
@@ -21,25 +23,11 @@ interface ClassNodeProps {
 }
 
 export default function ClassNode({ id, data }: ClassNodeProps) {
+  const dispatch = useStoreDispatch();
   const globalSelected = useSelector(selectSelected());
   const globalHover = useSelector(selectHovered());
-  const [showAtttributes, setShowAttributes] = useState(true);
+  const [showAttributes, setShowAttributes] = useState(true);
   const [hover, setHover] = useState(false);
-
-  const mockAttributes = [
-    {
-      identifier: `${id}-attr-1`,
-      label: 'Attribute #1',
-    },
-    {
-      identifier: `${id}-attr-2`,
-      label: 'Attribute #2',
-    },
-    {
-      identifier: `${id}-attr-3`,
-      label: 'Attribute #3',
-    },
-  ];
 
   return (
     <ClassNodeDiv
@@ -55,22 +43,24 @@ export default function ClassNode({ id, data }: ClassNodeProps) {
       <Handle type="target" position={Position.Top} id={id} />
       <div className="node-title">
         <div>{data.label}</div>
-        <button onClick={() => setShowAttributes(!showAtttributes)}>
+        <button onClick={() => setShowAttributes(!showAttributes)}>
           <Icon
-            icon={showAtttributes ? 'chevronUp' : 'chevronDown'}
+            icon={showAttributes ? 'chevronUp' : 'chevronDown'}
             variant="secondaryNoBorder"
           />
         </button>
       </div>
       <Handle type="source" position={Position.Bottom} id={id} />
 
-      {showAtttributes &&
-        mockAttributes.map((child) => (
+      {showAttributes &&
+        data.resources &&
+        data.resources.map((r) => (
           <Attribute
-            key={`${id}-child-${child.identifier}`}
+            key={`${id}-child-${r.identifier}`}
             className="node-resource"
+            onClick={() => dispatch(setSelected(r.identifier, 'attributes'))}
           >
-            {child.label}
+            {r.label}
           </Attribute>
         ))}
     </ClassNodeDiv>
