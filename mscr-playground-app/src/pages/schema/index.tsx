6ec0a0fc3406fdcc37d1b82eb0c23f33b3a23fa3
rquazi <rumana.quazi@csc.fi>
2023-05-19 15:37:22 +0300

    add inputs for schema upload

--- mscr-playground-app/src/pages/schema/index.tsx
+++ mscr-playground-app/src/pages/schema/index.tsx
@@ -1,8 +1,7 @@
 import EditCollection from '@app/modules/edit-collection';
-import { FormTitle } from '@app/modules/edit-vocabulary/edit-vocabulary.styles';
-import { PageContent } from '@app/modules/own-information/own-information.styles';
 import { SSRConfig } from 'next-i18next';
 import React from 'react';
+import { Button, Paragraph, TextInput } from 'suomifi-ui-components';
 import {
   CommonContextProvider,
   CommonContextState,
@@ -13,11 +12,37 @@ interface SchemaIndexPageProps extends CommonContextState {
   _netI18Next: SSRConfig;
 }
 
+const schemaFileUplaod = () => {
+  // Open the modal for file chooser, may be concept import modal can be used
+  console.log('Upload the schema file');
+};
+
 export default function Schemas(props: SchemaIndexPageProps) {
   return (
     <CommonContextProvider value={props}>
       <Layout user={props.user} fakeableUsers={props.fakeableUsers}>
-        <FormTitle></FormTitle>
+        <h2>Add New Schema Metadata</h2>
+        <TextInput
+          onBlur={(event) => console.log(event.target.value)}
+          labelText="Name"
+          visualPlaceholder="input a name for the schema"
+        />
+        <TextInput
+          onBlur={(event) => console.log(event.target.value)}
+          labelText="Description"
+          visualPlaceholder="Description of the schema"
+        />
+        <TextInput
+          onBlur={(event) => console.log(event.target.value)}
+          labelText="Filename"
+          visualPlaceholder="filename for the schema"
+        />
+        <Paragraph>
+          <Button onClick={() => schemaFileUplaod()} id="submit-button">
+            {'Register schema'}
+          </Button>
+        </Paragraph>
+
         <h2> List of Schemas</h2>
         <EditCollection
           terminologyId={'1'}
