2349d42245fd2d98230e4736c75c707538f971a2
Kalle Kyttälä <kalle.kyttala@rebase.fi>
2023-06-07 12:43:17 +0300

    change terms' order

---
+++ terminology-ui/src/modules/concept/utils.test.tsx
@@ -0,0 +1,126 @@
+import { getBlockData } from './utils';
+
+describe('order concept data', () => {
+  it('should order terms', () => {
+    const x = '';
+
+    const data = getBlockData(
+      jest.fn((x) => x),
+      concept
+    );
+    const termLabels = data.terms.map(
+      (t) => t.term.properties.prefLabel?.[0].value
+    );
+    const defintions = data.definitions.map((def) => def.value);
+
+    const expectedTermOrder = [
+      'finnish pref term',
+      'finnish synonym',
+      'finnish not recommended',
+      'swedish pref term',
+      'swedish synonym',
+      'german pref term',
+    ];
+
+    const expectedDefinitionOrder = [
+      'finnish definition',
+      'swedish definition',
+    ];
+
+    expect(JSON.stringify(termLabels)).toBe(JSON.stringify(expectedTermOrder));
+    expect(JSON.stringify(defintions)).toBe(
+      JSON.stringify(expectedDefinitionOrder)
+    );
+  });
+});
+
+const concept = {
+  id: '123',
+  properties: {
+    definition: [
+      {
+        lang: 'sv',
+        value: 'swedish definition',
+      },
+      {
+        lang: 'fi',
+        value: 'finnish definition',
+      },
+    ],
+  },
+  referrers: {},
+  references: {
+    altLabelXl: [
+      {
+        properties: {
+          prefLabel: [
+            {
+              lang: 'fi',
+              value: 'finnish synonym',
+              regex: '(?s)^.*$',
+            },
+          ],
+        },
+      },
+      {
+        properties: {
+          prefLabel: [
+            {
+              lang: 'sv',
+              value: 'swedish synonym',
+              regex: '(?s)^.*$',
+            },
+          ],
+        },
+      },
+    ],
+    prefLabelXl: [
+      {
+        properties: {
+          prefLabel: [
+            {
+              lang: 'fi',
+              value: 'finnish pref term',
+              regex: '(?s)^.*$',
+            },
+          ],
+        },
+      },
+      {
+        properties: {
+          prefLabel: [
+            {
+              lang: 'de',
+              value: 'german pref term',
+              regex: '(?s)^.*$',
+            },
+          ],
+        },
+      },
+      {
+        properties: {
+          prefLabel: [
+            {
+              lang: 'sv',
+              value: 'swedish pref term',
+              regex: '(?s)^.*$',
+            },
+          ],
+        },
+      },
+    ],
+    notRecommendedSynonym: [
+      {
+        properties: {
+          prefLabel: [
+            {
+              lang: 'fi',
+              value: 'finnish not recommended',
+              regex: '(?s)^.*$',
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
