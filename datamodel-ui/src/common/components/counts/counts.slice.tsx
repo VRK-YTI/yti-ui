8328b6fd2a7902f680e79e1ce3981fc8a3bdbc7b
Tomi Matsumoto-Viljanen <tomi.matsumoto-viljanen@gofore.com>
2023-06-26 11:28:07 +0300

    count query updates based on filters

--- datamodel-ui/src/common/components/counts/counts.slice.tsx
+++ datamodel-ui/src/common/components/counts/counts.slice.tsx
@@ -2,6 +2,40 @@ import { HYDRATE } from 'next-redux-wrapper';
 import { createApi } from '@reduxjs/toolkit/query/react';
 import { getDatamodelApiBaseQuery } from '@app/store/api-base-query';
 import { CountsType } from '@app/common/interfaces/counts.interface';
+import { UrlState } from 'yti-common-ui/utils/hooks/use-url-state';
+
+function getUrl(urlState: UrlState) {
+  if (!urlState) {
+    return '/frontend/counts';
+  }
+
+  const validEntries = Object.entries({
+    query: urlState.q,
+    language: urlState.lang ?? null,
+    organizations: urlState.organization ? [urlState.organization] : [],
+    groups: urlState.domain,
+    type: urlState.types
+      ? urlState.types.map((type) => type.toUpperCase())
+      : [],
+    ...(urlState.status.length === 0
+      ? { status: ['VALID', 'DRAFT'] }
+      : { status: urlState.status }),
+  }).filter(
+    (item) =>
+      item[1] &&
+      item[1] !== null &&
+      item[1] !== undefined &&
+      (Array.isArray(item[1])
+        ? item[1].length > 0
+        : typeof item[1] !== 'undefined')
+  );
+
+  const uri = `/frontend/counts?${validEntries
+    .map((e) => `${e[0]}=${Array.isArray(e[1]) ? e[1].join(',') : e[1]}`)
+    .join('&')}`;
+
+  return uri;
+}
 
 export const countApi = createApi({
   reducerPath: 'count',
@@ -18,10 +52,10 @@ export const countApi = createApi({
         totalHitCount: number;
         counts: CountsType;
       },
-      void
+      UrlState
     >({
-      query: () => ({
-        url: '/frontend/counts',
+      query: (urlState) => ({
+        url: getUrl(urlState),
         method: 'GET',
       }),
     }),
