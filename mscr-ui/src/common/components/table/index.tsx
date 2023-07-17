65ae5353cb6673acab566e0f340b5068268f69d0
username-u <sopranoluzhin@gmail.com>
2023-07-17 15:28:26 +0300

    added placeholder for smaller side bar, styled and changed content of the table, changed style of side navigation

--- mscr-ui/src/common/components/table/index.tsx
+++ mscr-ui/src/common/components/table/index.tsx
@@ -6,55 +6,56 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { TableWrapper } from './table.styles';
+import { useBreakpoints } from 'yti-common-ui/media-query';
 
 function createData(
   name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
+  status: string,
+  version: string,
+  pid: string
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { name, status, version, pid };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('MARC to MODS', 'Not registered', 'draft', '1.1.1.1'),
+  createData('DTC to EML', 'Registered', '1.1', '2.2.2.2'),
+  createData('DataCite to Dublin Core', 'Registered', '1.1', '3.3.3.3'),
+  createData('Type to Othertype', 'Registered', '1.1', '4.4.4.4'),
 ];
 
 export default function BasicTable() {
+  const { breakpoint } = useBreakpoints();
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    <TableWrapper $breakpoint={breakpoint} id="table-main">
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name </TableCell>
+              <TableCell align="left">Status</TableCell>
+              <TableCell align="left">Version</TableCell>
+              <TableCell align="left">PID</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="left">{row.status}</TableCell>
+                <TableCell align="left">{row.version}</TableCell>
+                <TableCell align="left">{row.pid}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </TableWrapper>
   );
 }
