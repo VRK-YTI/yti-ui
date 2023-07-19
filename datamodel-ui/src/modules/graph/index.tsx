eade5775693923d8bee5efa5293ca8bb4499e329
Kalle Kyttälä <kalle.kyttala@rebase.fi>
2023-07-19 13:03:47 +0300

    change visualization response type

--- datamodel-ui/src/modules/graph/index.tsx
+++ datamodel-ui/src/modules/graph/index.tsx
@@ -163,9 +163,14 @@ const GraphContent = ({ modelId, children }: GraphProps) => {
 
   useEffect(() => {
     if (isSuccess) {
-      setNodes(convertToNodes(data, i18n.language));
+      setNodes(convertToNodes(data.nodes, i18n.language));
       setEdges(
-        generateInitialEdges(data, deleteEdgeById, splitEdge, i18n.language)
+        generateInitialEdges(
+          data.nodes,
+          deleteEdgeById,
+          splitEdge,
+          i18n.language
+        )
       );
     }
   }, [
