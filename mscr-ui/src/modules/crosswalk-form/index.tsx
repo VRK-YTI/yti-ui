fe5685ff66326dd4fa786670074c19832a28f278
rquazi <rumana.quazi@csc.fi>
2023-08-11 15:13:15 +0300

    Text change

--- mscr-ui/src/modules/crosswalk-form/index.tsx
+++ mscr-ui/src/modules/crosswalk-form/index.tsx
@@ -96,6 +96,7 @@ export default function CrosswalkForm({
       <Separator isLarge />
       {editMode && renderContributors()}
       {renderStaus()}
+      <Separator />
     </ModelFormContainer>
   );
 
