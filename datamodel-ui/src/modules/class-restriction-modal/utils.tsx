f9507f3e6a838fd82285b8a28c3120a925511b4b
Tomi Matsumoto-Viljanen <tomi.matsumoto-viljanen@gofore.com>
2023-10-17 14:08:06 +0300

    add radiobutton for selected class

---
+++ datamodel-ui/src/modules/class-restriction-modal/utils.tsx
@@ -0,0 +1,53 @@
+import { ResultType } from '@app/common/components/resource-list';
+import { InternalClassInfo } from '@app/common/interfaces/internal-class.interface';
+import { getLanguageVersion } from '@app/common/utils/get-language-version';
+
+export function mapInternalClassInfoToResultType(
+  data: InternalClassInfo,
+  language: string
+): ResultType {
+  return {
+    ...(data.conceptInfo && {
+      concept: {
+        label: getLanguageVersion({
+          data: data.conceptInfo.conceptLabel,
+          lang: language,
+          appendLocale: true,
+        }),
+        link: data.conceptInfo.conceptURI,
+        partOf: getLanguageVersion({
+          data: data.conceptInfo?.terminologyLabel,
+          lang: language,
+          appendLocale: true,
+        }),
+      },
+    }),
+    partOf: {
+      domains: data.dataModelInfo.groups,
+      label: getLanguageVersion({
+        data: data.dataModelInfo.label,
+        lang: language,
+        appendLocale: true,
+      }),
+      type: data.dataModelInfo.modelType,
+      uri: data.dataModelInfo.uri,
+    },
+    target: {
+      identifier: data.id,
+      label: getLanguageVersion({
+        data: data.label,
+        lang: language,
+        appendLocale: true,
+      }),
+      link: data.id,
+      linkLabel: data.curie,
+      note: getLanguageVersion({
+        data: data.note,
+        lang: language,
+        appendLocale: true,
+      }),
+      status: data.status,
+      isValid: data.status === 'VALID',
+    },
+  };
+}
