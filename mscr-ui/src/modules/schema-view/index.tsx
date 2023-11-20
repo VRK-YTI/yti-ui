d9f6933cc3f0884ee5fd84d41bc2a123ee20821c
Maaria Wahlström <maaria.wahlstrom@csc.fi>
2023-11-20 17:21:23 +0200

    Implement schema metadata view

--- mscr-ui/src/modules/schema-view/index.tsx
+++ mscr-ui/src/modules/schema-view/index.tsx
@@ -4,8 +4,14 @@ import Tab from '@mui/material/Tab';
 import { SyntheticEvent, useState } from 'react';
 import MetadataAndFiles from '@app/modules/schema-view/metadata-and-files';
 import VersionHistory from '@app/modules/schema-view/version-history';
+import {useTranslation} from 'next-i18next';
+import {useGetSchemaQuery} from '@app/common/components/schema/schema.slice';
 
 export default function SchemaView({ schemaId }: { schemaId: string }) {
+  const { t } = useTranslation('common');
+
+  const { data: schemaDetails, isLoading, isSuccess, isError, error } =
+    useGetSchemaQuery(schemaId);
   const [selectedTab, setSelectedTab] = useState(0);
 
 
@@ -19,21 +25,35 @@ export default function SchemaView({ schemaId }: { schemaId: string }) {
   const changeTab = (event: SyntheticEvent | undefined, newValue: number) => {
     setSelectedTab(newValue);
   };
+  if (isLoading) {
+    return (
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  } else if (isSuccess) {
+    return (
+      <>
+        <Box className='mb-3' sx={{borderBottom: 1, borderColor: 'divider'}}>
+          <Tabs value={selectedTab} onChange={changeTab} aria-label="Category selection">
+            <Tab label={t('schema.metadata-and-files')} {...a11yProps(0)} />
+            <Tab label={t('schema.version-history')} {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        {selectedTab === 0 &&
+            <MetadataAndFiles schemaDetails={schemaDetails} />
+        }
+        {selectedTab === 1 &&
+            <VersionHistory />
+        }
+      </>
+    );
+  }
 
+  // TODO: What to return if data fetching returns error?
   return (
-    <>
-      <Box className='mb-3' sx={{borderBottom: 1, borderColor: 'divider'}}>
-        <Tabs value={selectedTab} onChange={changeTab} aria-label="Category selection">
-          <Tab label="Metadata & files" {...a11yProps(0)} />
-          <Tab label="Version history" {...a11yProps(1)} />
-        </Tabs>
-      </Box>
-      {selectedTab === 0 &&
-        <MetadataAndFiles />
-      }
-      {selectedTab === 1 &&
-        <VersionHistory />
-      }
-    </>
+    <></>
   );
 }
--- mscr-ui/src/modules/schema-view/metadata-and-files/index.tsx
+++ mscr-ui/src/modules/schema-view/metadata-and-files/index.tsx
@@ -1,7 +1,91 @@
-export default function MetadataAndFiles() {
+import {useTranslation} from 'next-i18next';
+import {useMemo} from 'react';
+import {Schema} from '@app/common/interfaces/schema.interface';
+import router from 'next/router';
+
+export default function MetadataAndFiles({ schemaDetails }: { schemaDetails?: Schema }) {
+  const { t } = useTranslation('common');
+  const lang = router.query.lang;
+
+  // TODO: Only edit with permission, we have util has-permission
+  // TODO: Get organization names neatly
+  // Locale: Object.entries(schemaDetails.label).find((t) => t[0] === lang)?.[1] ?? ''
+  // Organization: see datamodel-ui/src/modules/model/model-info-view.tsx
+  interface SchemaDisplay {
+    [key:string]: string;
+  }
+  const schemaDisplay : SchemaDisplay = useMemo(() => {
+    if (!schemaDetails) {
+      return (
+        {
+          schemaPid: '',
+          schemaLabel: '',
+          schemaDescription: '',
+          schemaCreated: '',
+          schemaModified: '',
+          schemaState: '',
+          schemaOrganizations: '',
+          schemaVisibility: '',
+          schemaFormat: '',
+          schemaVersionLabel: '',
+          schemaNamespace: ''
+        }
+      );
+    }
+
+    return (
+      {
+        schemaPid: schemaDetails?.pid ?? '',
+        schemaLabel: schemaDetails?.label ? Object.entries(schemaDetails.label).find((t) => t[0] === lang)?.[1] ?? '' : '',
+        schemaDescription: schemaDetails?.description ? Object.entries(schemaDetails.description).find((t) => t[0] === lang)?.[1] ?? '' : '',
+        schemaCreated: schemaDetails?.created ?? '',
+        schemaModified: schemaDetails?.modified ?? '',
+        schemaState: schemaDetails?.state ?? '',
+        schemaOrganizations: schemaDetails?.organizations.toString() ?? '',
+        schemaVisibility: schemaDetails?.visibility ?? '',
+        schemaFormat: schemaDetails?.format ?? '',
+        schemaVersionLabel: schemaDetails?.versionLabel ?? '',
+        schemaNamespace: schemaDetails?.namespace ?? ''
+      }
+    );
+  }, [schemaDetails, lang]);
+
   return (
     <>
-      Metadata and files
+      <dl>
+        <dt>
+          {t('schema.name')}
+        </dt>
+        <dd>
+          {schemaDisplay.schemaLabel}
+        </dd>
+      </dl>
+      <div className='crosswalk-editor mx-2'>
+        <h2 className='mt-4 mb-3'>Crosswalk details</h2>
+        <div className='row d-flex justify-content-between metadata-and-files-wrap mx-2'>
+          <div className='row bg-light-blue'>
+            <div className='col-6'>
+              <div className='my-3'> Name: <span></span></div>
+              <div className='my-3'> Description: <span>{schemaDisplay.schemaDescription}</span></div>
+              <div className='my-3'> PID: <span>{schemaDisplay.schemaPid}</span></div>
+              <div className='my-3'> Version: <span>{schemaDisplay.schemaVersionLabel}</span></div>
+              <div className='my-3'> Created: <span>{schemaDisplay.schemaCreated}</span></div>
+              <div className='my-3'> Modified: <span>{schemaDisplay.schemaModified}</span></div>
+              <div className='my-3'> State: <span>{schemaDisplay.schemaState}</span></div>
+              <div className='my-3'> Visibility: <span>{schemaDisplay.schemaVisibility}</span></div>
+              <div className='my-3'> Format: <span>{schemaDisplay.schemaFormat}</span></div>
+              <div className='my-3'> Namespace: <span>{schemaDisplay.schemaNamespace}</span></div>
+              <div className='my-3'> Organizations: <span>{schemaDisplay.schemaOrganizations}</span></div>
+            </div>
+          </div>
+        </div>
+        <h2 className='mt-4 mb-3'>Files</h2>
+        <div className='row d-flex justify-content-between metadata-and-files-wrap mx-2'>
+          <br/>
+
+          <div>TABLE HERE{/* TODO: Display schema files */}</div>
+        </div>
+      </div>
     </>
   );
 }
