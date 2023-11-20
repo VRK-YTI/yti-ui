4325cf0d1827b007046b30129a0856ff1df6ae68
Maaria Wahlström <maaria.wahlstrom@csc.fi>
2023-11-20 16:04:48 +0200

    Add tabs for schema info and version history

--- mscr-ui/src/modules/schema-view/index.tsx
+++ mscr-ui/src/modules/schema-view/index.tsx
@@ -1,6 +1,29 @@
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import {SyntheticEvent, useState} from 'react';
+
 export default function SchemaView({ schemaId }: { schemaId: string }) {
+  const [selectedTab, setSelectedTab] = useState(0);
+
+
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+  const changeTab = (event: SyntheticEvent | undefined, newValue: number) => {
+    setSelectedTab(newValue);
+  };
+
   return (
-    <>
-    </>
+    <Box className='mb-3' sx={{borderBottom: 1, borderColor: 'divider'}}>
+      <Tabs value={selectedTab} onChange={changeTab} aria-label="Category selection">
+        <Tab label="Metadata & files" {...a11yProps(0)} />
+        <Tab label="Version history" {...a11yProps(1)} />
+      </Tabs>
+    </Box>
   );
 }
