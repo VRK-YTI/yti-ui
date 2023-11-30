01593250df3a001b93a8dfd6293f4524cb964443
topirissanen-atoz <topi.rissanen@atoz.fi>
2023-11-30 09:11:48 +0200

    Hiding attributes and associations in graph improved

---
+++ datamodel-ui/src/modules/graph/utils/get-start-node/index.tsx
@@ -0,0 +1,41 @@
+import { Edge, Node } from 'reactflow';
+
+export default function getStartNode(
+  startElement: Node | Edge,
+  nodes: Node[],
+  edges: Edge[]
+) {
+  let start: Node | undefined;
+
+  if ('source' in startElement) {
+    start = nodes.find((node) => node.id === startElement.source);
+  } else {
+    start = startElement;
+  }
+
+  if (!start) {
+    return undefined;
+  }
+
+  return getSource(start, edges, nodes);
+}
+
+function getSource(start: Node, edges: Edge[], nodes: Node[]) {
+  if (!start) {
+    return;
+  }
+
+  if (start.type !== 'cornerNode') {
+    return start;
+  }
+
+  const parent = nodes.find(
+    (node) => node.id === edges.find((edge) => edge.target === start.id)?.source
+  );
+
+  if (!parent) {
+    return;
+  }
+
+  return getSource(parent, edges, nodes);
+}
