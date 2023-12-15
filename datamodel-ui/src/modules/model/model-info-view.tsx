b5d1156869c6a39dda2ab299095097faf9960069
Tomi Matsumoto-Viljanen <tomi.matsumoto-viljanen@gofore.com/>
2023-12-15 10:25:23 +0200

    add to draft button

--- datamodel-ui/src/modules/model/model-info-view.tsx
+++ datamodel-ui/src/modules/model/model-info-view.tsx
@@ -50,6 +50,7 @@ export default function ModelInfoView({
   organizationIds?: string[];
 }) {
   const { t, i18n } = useTranslation('common');
+  const router = useRouter();
   const dispatch = useStoreDispatch();
   const { query } = useRouter();
   const [modelId] = useState(getSlugAsString(query.slug) ?? '');
@@ -168,29 +169,34 @@ export default function ModelInfoView({
           <Text variant="bold">{t('details')}</Text>
           <ActionMenu id="actions-menu" buttonText={t('actions')}>
             {hasPermission ? (
-              <>
-                <ActionMenuItem
-                  onClick={() => handleEditViewItemClick(setShowEditView)}
-                  disabled={!formData}
-                >
-                  {t('edit', { ns: 'admin' })}
-                </ActionMenuItem>
-                <ActionMenuDivider />
-              </>
-            ) : (
-              <></>
-            )}
-            {hasPermission && !version ? (
               <ActionMenuItem
-                onClick={() => handleModalChange('createRelease', true)}
+                onClick={() => handleEditViewItemClick(setShowEditView)}
                 disabled={!formData}
               >
-                {t('create-release', { ns: 'admin' })}
+                {t('edit', { ns: 'admin' })}
               </ActionMenuItem>
             ) : (
               <></>
             )}
-
+            {hasPermission ? (
+              version ? (
+                <ActionMenuItem
+                  onClick={() => router.push(`/model/${modelId}`)}
+                >
+                  {t('to-draft', { ns: 'admin' })}
+                </ActionMenuItem>
+              ) : (
+                <ActionMenuItem
+                  onClick={() => handleModalChange('createRelease', true)}
+                  disabled={!formData}
+                >
+                  {t('create-release', { ns: 'admin' })}
+                </ActionMenuItem>
+              )
+            ) : (
+              <></>
+            )}
+            {hasPermission ? <ActionMenuDivider /> : <></>}
             <ActionMenuItem
               onClick={() => handleModalChange('showAsFile', true)}
             >
