36affd3bf54dc7fdc323feec4c3b85ad4af5d3f5
Maaria Wahlström <maaria.wahlstrom@csc.fi>
2024-02-23 14:04:45 +0200

    MSCR-360 Clean up content pages' getServerSideProps

--- mscr-ui/src/pages/schema/[...pid].tsx
+++ mscr-ui/src/pages/schema/[...pid].tsx
@@ -5,16 +5,6 @@ import {
 import Layout from '@app/common/components/layout';
 import { SSRConfig } from 'next-i18next';
 import { createCommonGetServerSideProps } from '@app/common/utils/create-getserversideprops';
-import {
-  getServiceCategories,
-  getRunningQueriesThunk as getServiceQueriesThunk,
-} from '@app/common/components/service-categories/service-categories.slice';
-import {
-  getOrganizations,
-  getRunningQueriesThunk as getOrgQueriesThunk,
-} from '@app/common/components/organizations/organizations.slice';
-import { getRunningQueriesThunk as getInternalResourcesRunningQueriesThunk } from '@app/common/components/search-internal-resources/search-internal-resources.slice';
-import { getRunningQueriesThunk as getVisualizationRunningQueriesThunk } from '@app/common/components/visualization/visualization.slice';
 import { useRouter } from 'next/router';
 import { MscrUser } from '@app/common/interfaces/mscr-user.interface';
 import SchemaView from '@app/modules/schema-view';
@@ -41,20 +31,4 @@ export default function SchemaPage(props: IndexPageProps) {
   );
 }
 
-export const getServerSideProps = createCommonGetServerSideProps(
-  async ({ store, query, locale }) => {
-    store.dispatch(getServiceCategories.initiate(locale ?? 'fi'));
-    store.dispatch(getOrganizations.initiate(locale ?? 'en'));
-
-    await Promise.all(store.dispatch(getServiceQueriesThunk()));
-    await Promise.all(store.dispatch(getOrgQueriesThunk()));
-    await Promise.all(
-      store.dispatch(getInternalResourcesRunningQueriesThunk())
-    );
-    await Promise.all(store.dispatch(getVisualizationRunningQueriesThunk()));
-
-    return {
-      props: {},
-    };
-  }
-);
+export const getServerSideProps = createCommonGetServerSideProps();
