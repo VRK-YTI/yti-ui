783c339270b44214573226acf93a05bb171ca0c1
konolak <kari.konola@csc.fi>
2024-04-18 12:25:02 +0300

    format code and add heading styles to version history

--- mscr-ui/src/common/components/version-history/index.tsx
+++ mscr-ui/src/common/components/version-history/index.tsx
@@ -1,14 +1,15 @@
-import { useTranslation } from 'next-i18next';
-import { ContentRevision } from '@app/common/interfaces/content-revision.interface';
+import {useTranslation} from 'next-i18next';
+import {ContentRevision} from '@app/common/interfaces/content-revision.interface';
 import FormattedDate from 'yti-common-ui/components/formatted-date';
 import GenericTable from "@app/common/components/generic-table";
+import {VersionHistoryContainer} from "@app/common/components/version-history/version-history.styles";
 
 export default function VersionHistory({
-  revisions,
-}: {
+                                         revisions,
+                                       }: {
   revisions: ContentRevision[];
 }) {
-  const { t } = useTranslation('common');
+  const {t} = useTranslation('common');
   const headers = [
     t('metadata.version-label'),
     t('metadata.pid'),
@@ -16,10 +17,15 @@ export default function VersionHistory({
     t('metadata.state'),
   ];
 
-  const revisionsFormatted = revisions.map((revision) => ({versionLabel: revision.versionLabel, pid: revision.pid, created: <FormattedDate date={revision.created}/>, state: revision.state}));
+  const revisionsFormatted = revisions.map((revision) => ({
+    versionLabel: revision.versionLabel,
+    pid: revision.pid,
+    created: <FormattedDate date={revision.created}/>,
+    state: revision.state
+  }));
 
   return (
-    <><GenericTable items={revisionsFormatted} headings={headers}
-                    caption={t('metadata.versions')}></GenericTable></>
+    <><VersionHistoryContainer><GenericTable items={revisionsFormatted} headings={headers}
+                                             caption={t('metadata.versions')}></GenericTable></VersionHistoryContainer></>
   );
 }
---
+++ mscr-ui/src/common/components/version-history/version-history.styles.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const VersionHistoryContainer = styled.div`
+  color: ${(props) => props.theme.suomifi.colors.blackBase};
+  margin: 0.5rem;
+  h2 {
+    color: #6b6b6b;
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+`;
