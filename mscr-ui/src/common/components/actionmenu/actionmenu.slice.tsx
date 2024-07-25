8ebcafd14e4d7382403365a720cd18c88b0ff135
Maaria Wahlström <maaria.wahlstrom@csc.fi>
2024-07-25 10:29:01 +0300

    MSCR-397 Remove unused code

--- mscr-ui/src/common/components/actionmenu/actionmenu.slice.tsx
+++ mscr-ui/src/common/components/actionmenu/actionmenu.slice.tsx
@@ -38,7 +38,7 @@ export interface FormState {
 
 const initialFormState: FormState = {
   mscrCopy: false,
-  version: false
+  version: false,
 };
 
 export interface ConfirmState {
@@ -65,8 +65,6 @@ const initialModalList: ModalList = {
 };
 
 const initialState = {
-  isLatest: false,
-  isPublished: false,
   isCrosswalk: false,
   menuList: initialMenuList,
   modal: initialModalList,
@@ -76,39 +74,29 @@ export const actionmenuSlice = createSlice({
   name: 'actionmenu',
   initialState: initialState,
   reducers: {
-    setIsLatest(state, action) {
-      return {
-        ...state,
-        isLatest: action.payload
-      };
-    },
-    setIsPublished(state, action) {
-      return {
-        ...state,
-        isPublished: action.payload
-      };
-    },
     setIsCrosswalk(state, action) {
       return {
         ...state,
-        isCrosswalk: action.payload
+        isCrosswalk: action.payload,
       };
     },
     setMenuList(state, action) {
       const menuListAddition: Partial<MenuList> = {};
-      action.payload.forEach((key: keyof MenuList) => menuListAddition[key] = true);
+      action.payload.forEach(
+        (key: keyof MenuList) => (menuListAddition[key] = true)
+      );
       return {
         ...state,
         menuList: {
           ...state.menuList,
-          ...menuListAddition
-        }
+          ...menuListAddition,
+        },
       };
     },
     setMenuListFull(state, action) {
       return {
         ...state,
-        menuList: action.payload
+        menuList: action.payload,
       };
     },
     setFormState(state, action) {
@@ -118,9 +106,9 @@ export const actionmenuSlice = createSlice({
           ...initialModalList,
           form: {
             ...initialFormState,
-            [action.payload.key]: action.payload.value
-          }
-        }
+            [action.payload.key]: action.payload.value,
+          },
+        },
       };
     },
     setConfirmState(state, action) {
@@ -130,31 +118,13 @@ export const actionmenuSlice = createSlice({
           ...initialModalList,
           confirm: {
             ...initialConfirmState,
-            [action.payload.key]: action.payload.value
-          }
-        }
+            [action.payload.key]: action.payload.value,
+          },
+        },
       };
     },
   },
 });
-
-export function selectIsLatest() {
-  return (state: AppState) => state.actionmenu.isLatest;
-}
-
-export function setIsLatest(isLatest?: boolean): AppThunk {
-  return (dispatch) =>
-    dispatch(actionmenuSlice.actions.setIsLatest(isLatest ?? false));
-}
-
-export function selectIsPublished() {
-  return (state: AppState) => state.actionmenu.isPublished;
-}
-
-export function setIsPublished(isPublished?: boolean): AppThunk {
-  return (dispatch) =>
-    dispatch(actionmenuSlice.actions.setIsPublished(isPublished ?? false));
-}
 
 export function selectIsCrosswalk() {
   return (state: AppState) => state.actionmenu.isCrosswalk;
@@ -174,7 +144,8 @@ export function setMenuList(menuList: Array<keyof MenuList>): AppThunk {
 }
 
 export function resetMenuList(): AppThunk {
-  return (dispatch) => dispatch(actionmenuSlice.actions.setMenuListFull(initialMenuList));
+  return (dispatch) =>
+    dispatch(actionmenuSlice.actions.setMenuListFull(initialMenuList));
 }
 
 export function selectModal() {
@@ -185,7 +156,10 @@ export function selectFormModalState() {
   return (state: AppState) => state.actionmenu.modal.form;
 }
 
-export function setFormModalState(formType: {key: keyof FormState; value: boolean}): AppThunk {
+export function setFormModalState(formType: {
+  key: keyof FormState;
+  value: boolean;
+}): AppThunk {
   return (dispatch) => dispatch(actionmenuSlice.actions.setFormState(formType));
 }
 
@@ -193,6 +167,10 @@ export function selectConfirmModalState() {
   return (state: AppState) => state.actionmenu.modal.confirm;
 }
 
-export function setConfirmModalState(confirmType: {key: keyof ConfirmState; value: boolean}): AppThunk {
-  return (dispatch) => dispatch(actionmenuSlice.actions.setConfirmState(confirmType));
+export function setConfirmModalState(confirmType: {
+  key: keyof ConfirmState;
+  value: boolean;
+}): AppThunk {
+  return (dispatch) =>
+    dispatch(actionmenuSlice.actions.setConfirmState(confirmType));
 }
