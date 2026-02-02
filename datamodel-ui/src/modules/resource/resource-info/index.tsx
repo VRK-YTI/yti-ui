5d99d5c65b323f6c5217ce31ba04334c7901a59e
Joonas Kaski <joonas.kaski@gofore.com>
2026-02-02 20:37:11 +0200

    remove redundant function call from delete modal in resourceinfo

--- datamodel-ui/src/modules/resource/resource-info/index.tsx
+++ datamodel-ui/src/modules/resource/resource-info/index.tsx
@@ -196,7 +196,6 @@ export default function ResourceInfo({
                 })}
                 onClose={() => {
                   setDeleteVisible(false);
-                  refetchResourceList();
                   handleReturn();
                 }}
                 visible={deleteVisible}
